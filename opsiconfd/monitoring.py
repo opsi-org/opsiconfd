f16f5f278efc96f0d2f4d76b222a48dd8194e5c4
Erol Ülükmen <e.ueluekmen@uib.de>
2011-04-04 13:57:19 +0000

    -

--- opsiconfd/monitoring.py
+++ opsiconfd/monitoring.py
@@ -97,6 +97,7 @@ class WorkerOpsiconfdMonitoring(WorkerOpsi):
 			if not query.has_key("task"):
 				raise Exception("No task set, nothing to do") 
 			print ">>>>>>>>>>>>>",query["task"]
+				
 			if query["task"] == "checkClientStatus":
 				
 				if query["param"]:
@@ -117,25 +118,41 @@ class WorkerOpsiconfdMonitoring(WorkerOpsi):
 						return result
 						
 			elif query["task"] == "checkProductStatus":
-# def checkProductStatus(self, productIds = [], productGroups = [], depotIds = [], exclude=[]):
 				if query["param"]:
 					productIds = []
 					groupIds = [] 
 					depotIds = [] 
 					exclude=[]
+					verbose = False
 					if query["param"].has_key("productIds"):
 						productIds = query["param"]["productIds"]
-					elif query["param"].has_key("groupIds"):
+					if query["param"].has_key("groupIds"):
 						groupIds = query["param"]["groupIds"]
-					elif query["param"].has_key("depotIds"):
+					if query["param"].has_key("depotIds"):
 						depotIds = query["param"]["depotIds"]
-					elif query["param"].has_key("exclude"):
+					if query["param"].has_key("exclude"):
 						exclude = query["param"]["exclude"]
-					res = self.monitoring.checkProductStatus(productIds, groupIds, depotIds, exclude)
+					if query["param"].has_key("verbose"):
+						verbose = True
+					res = self.monitoring.checkProductStatus(productIds, groupIds, depotIds, exclude, verbose)
 					result.stream = stream.IByteStream(res.encode('utf-8'))
 					return result
 				else:
 					raise Exception(u"Failure: Parameterlist for task not complete, clientId needed for these check.")
+			elif query["task"] == "checkDepotSyncStatus":
+				if query["param"]:
+					depotIds = []
+					productIds = []
+					exclude = []
+				if query["param"].has_key("productIds"):
+					productIds = query["param"]["productIds"]
+				if query["param"].has_key("depotIds"):
+					depotIds = query["param"]["depotIds"]
+				if query["param"].has_key("exclude"):
+					exclude = query["param"]["exclude"]
+				res = self.monitoring.checkDepotSyncStatus(depotIds, productIds, exclude)
+				result.stream = stream.IByteStream(res.encode('utf-8'))
+				return result
 			else:
 				raise Exception(u"Failure: unknown task!")
 				
@@ -195,7 +212,6 @@ class Monitoring(object):
 		clients = []
 		result = {}
 		objectToGroups = self.backend.objectToGroup_getObjects(groupId=groups, type="HostGroup")
-		print objectToGroups
 		if objectToGroups:
 			for objectToGroup in objectToGroups:
 				clients.append(objectToGroup.objectId)
@@ -210,6 +226,203 @@ class Monitoring(object):
 						}
 		return json.dumps(result)
 	
+	def checkProductStatus(self, productIds = [], productGroups = [], depotIds = [], exclude=[], verbose = False):
+		state = self._OK
+		clientsOnDepot = {}
+		
+		actionRequestOnClient = {}
+		productProblemsOnClient = {}
+		productProblemsOnDepot = {}
+		productVersionProblemsOnClient = {}
+		
+		if not productIds:
+			productIds = []
+			for product in self.backend.objectToGroup_getIdents(groupType='ProductGroup',groupId=productGroups).split(";")[2]:
+				if not product in productIds:
+					productIds.append(product)
+		if not depotIds or 'all' in depotIds:
+			depotIds = []
+			depots = self.backend.host_getObjects(type="OpsiDepotserver")
+			for depot in depots:
+				depotIds.append(depot.id)
+		
+		clientIds = self.backend.host_getIdents(type="OpsiClient")
+		addConfigStateDefaults = self.backend.backend_getOptions().get('addConfigStateDefaults', False)
+		try:
+			logger.debug("Calling backend_setOptions on %s" % self)
+			self.backend.backend_setOptions({'addConfigStateDefaults': True})
+			
+			for configState in self.backend.configState_getObjects(configId = u'clientconfig.depot.id', objectId = clientIds):
+				if not configState.values or not configState.values[0]:
+					logger.error(u"No depot server configured for client '%s'" % configState.objectId)
+					continue
+				depotId = configState.values[0]
+				if not depotId in depotIds:
+					continue
+				if not clientsOnDepot.has_key(depotId):
+					clientsOnDepot[depotId] = []
+				clientsOnDepot[depotId].append(configState.objectId)
+					
+		finally:
+			self.backend.backend_setOptions({'addConfigStateDefaults': addConfigStateDefaults})
+		
+		
+		
+		productOnDepotInfo = {}
+		for pod in self.backend.productOnDepot_getObjects(depotId = depotIds, productId = productIds):
+			if not productOnDepotInfo.has_key(pod.depotId):
+				productOnDepotInfo[pod.depotId] = {}
+				productOnDepotInfo[pod.depotId][pod.productId] = {	"productVersion": 	pod.productVersion,
+											"packageVersion":	pod.packageVersion }
+		for depotId in depotIds:
+			for poc in self.backend.productOnClient_getObjects(productId = productIds, clientId = clientsOnDepot[depotId]):
+				if poc.actionRequest != 'none':
+					if state != self._CRITICAL:
+						state = self._WARNING
+					if not actionRequestOnClient.has_key(depotId):
+						actionRequestOnClient[depotId] = {}
+					if not actionRequestOnClient[depotId].has_key(poc.productId):
+						actionRequestOnClient[depotId][poc.productId] = []
+					actionRequestOnClient[depotId][poc.productId].append(u"%s (%s)" % (poc.clientId, poc.actionRequest) )
+				if poc.installationStatus != "not_installed" and poc.actionResult != "successful": 
+					if state != self._CRITICAL:
+						state = self._CRITICAL
+					if not productProblemsOnClient.has_key(depotId):
+						productProblemsOnClient[depotId] = {}
+					if not productProblemsOnClient[depotId].has_key(poc.productId):
+						productProblemsOnClient[depotId][poc.productId] = []
+					productProblemsOnClient[depotId][poc.productId].append(u"%s (%s)" % (poc.clientId, poc.actionResult))
+				if not poc.productVersion or poc.packageVersion:
+					continue
+				if poc.productVersion != productOnDepotInfo[depotId][poc.productId]["productVersion"] or \
+					poc.packageVersion != productOnDepotInfo[depotId][poc.productId]["packageVersion"]:
+					if state != self._CRITICAL:
+						state = self._CRITICAL
+					if not productVersionProblemsOnClient.has_key(depotId):
+						productVersionProblemsOnClient[depotId] = {}
+					if not productVersionProblemsOnClient[depotId].has_key(poc.productId):
+						productVersionProblemsOnClient[depotId][poc.productId] = []
+					productVersionProblemsOnClient[depotId][poc.productId].append("%s (%s-%s)" % (poc.clientId,poc.productVersion, poc.packageVersion))
+		message = ''
+		if not verbose:
+			for depotId in depotIds:
+				message += "Result for Depot: '%s': " % depotId
+				if actionRequestOnClient.has_key(depotId):
+					for product in actionRequestOnClient[depotId].keys():
+						message += "For product '%s' action set on '%d' clients; " % (product, len(actionRequestOnClient[depotId][product]))
+				if productProblemsOnClient.has_key(depotId):
+					for product in productProblemsOnClient[depotId].keys():
+						message += "For product '%s' problems found on '%d' clients; " % (product, len(productProblemsOnClient[depotId][product]))
+				if productVersionProblemsOnClient.has_key(depotId):
+					for product in productVersionProblemsOnClient[depotId].keys():
+						message += "For product '%s' version defference problems found on '%d' clients; " % (product, len(productVersionProblemsOnClient[depotId][product]))
+			if state == self._OK:
+				message = u"No Problem found for productIds; '%s'" % productIds
+			return self._generateResponse(state, message)
+				
+		for depotId in depotIds:
+			message += "Result for Depot: '%s': " % depotId
+			if actionRequestOnClient.has_key(depotId):
+				message += "Action Request set for "
+				for product in actionRequestOnClient[depotId].keys():
+					message += "product '%s': " % product
+					for item in actionRequestOnClient[depotId][product]:
+						message += "%s " % item
+			if productProblemsOnClient.has_key(depotId):
+				message += "Product Problems for "
+				for product in productProblemsOnClient[depotId].keys():
+					message += "product '%s': " % product
+					for item in productProblemsOnClient[depotId][product]:
+						message += "%s " % item
+			if productVersionProblemsOnClient.has_key(depotId):
+				message += "Product Version difference found for: "
+				for product in productVersionProblemsOnClient[depotId].keys():
+					message += "product '%s': " % product
+					for item in productVersionProblemsOnClient[depotId][product]:
+						message += "%s " % item
+		
+		if state == self._OK:
+			message = u"No Problem found for productIds; '%s'" % "".join(productIds)
+			
+		return self._generateResponse(state, message)
+		
+	def checkDepotSyncStatus(self, depotIds, productIds = [], exclude = []):
+		state = self._OK
+		productOnDepotInfo = {}
+		differenceProducts = {}
+		
+		if not depotIds or 'all' in depotIds:
+			depotIds = []
+			depots = self.backend.host_getObjects(type="OpsiDepotserver")
+			for depot in depots:
+				depotIds.append(depot.id)
+		
+		productOnDepots = self.backend.productOnDepot_getObjects(depotId = depotIds, productId = productIds)
+		productIds = []
+		for depotId in depotIds:
+			productOnDepotInfo[depotId] = {}
+		for pod in productOnDepots:
+			if not pod.productId in productIds:
+				productIds.append(pod.productId)
+			productOnDepotInfo[pod.depotId][pod.productId] = pod
+		productIds.sort()
+		
+		for productId in productIds:
+			if productId in exclude:
+				continue
+			differs = False
+			productVersion = u''
+			packageVersion = u''
+			for depotId in depotIds:
+				productOnDepot = productOnDepotInfo[depotId].get(productId)
+				if not productOnDepot:
+					if not differenceProducts.has_key(productId): 
+						differenceProducts[productId] = {}
+					differenceProducts[productId][depotId] = "not installed"
+					continue
+				if not productVersion:
+					productVersion = productOnDepot.productVersion
+				elif (productVersion != productOnDepot.productVersion):
+					differs = True
+				if not packageVersion:
+					packageVersion = productOnDepot.packageVersion
+				elif (packageVersion != productOnDepot.packageVersion):
+					differs = True
+				
+				if differs:
+					if not differenceProducts.has_key(productId): 
+						differenceProducts[productId] = {}
+					differenceProducts[productId][depotId] = "different"
+		
+		message = u''
+		if differenceProducts:
+			state = self._WARNING
+			message += u"Differences found for "
+			
+			for productId in differenceProducts.keys():
+				message += u"product: '%s': " % productId
+				for depotId in depotIds:
+					if differenceProducts[productId].has_key(depotId):
+						if differenceProducts[productId][depotId] == "not installed":
+							message += u"%s (not installed) " % depotId
+						else:
+							message += u"%s (%s-%s) " % (depotId,
+								productOnDepotInfo[depotId][productId].productVersion,
+								productOnDepotInfo[depotId][productId].packageVersion)
+					else:
+						message += u"%s (%s-%s) " % (depotId,
+								productOnDepotInfo[depotId][productId].productVersion,
+								productOnDepotInfo[depotId][productId].packageVersion)	
+		else:
+			message += "Syncstate ok for depots: '%s' " % ",".join(depotIds)
+		
+		
+		print message
+		return self._generateResponse(state, message)
+			
+		
+	
+	'''
 	def checkProductStatus(self, productIds = [], productGroups = [], depotIds = [], exclude=[]):
 		productOnDepotInfo = {}
 		hostOnDepotInfo = {}
@@ -254,6 +467,8 @@ class Monitoring(object):
 		
 		configStates = self.backend.configState_getObjects(configId="clientconfig.depot.id")
 		
+		
+		
 		for configState in configStates:
 			hostOnDepotInfo[configState.objectId] = configState.values[0]
 			
@@ -267,8 +482,7 @@ class Monitoring(object):
 			if exclude:
 				if productOnClient.clientId in exclude:
 					continue
-			print ">>>>>>>>>>>>>>>",productOnClient.installationStatus
-			print ">>>>>>>>>>>>>>>",productOnClient.actionResult
+			
 			if productOnClient.installationStatus == "installed":
 				if productOnClient.actionResult:	
 					if not "success" in productOnClient.actionResult:
@@ -313,21 +527,21 @@ class Monitoring(object):
 					for clientId in productProblemsOnClient[productId]:
 						message += u"%s (%s) " % (clientId, productId)
 					
-		elif productProblemsOnDepot:
+		if productProblemsOnDepot:
 			message += u"Cannot check, because product not installed on depot: "
 			for productId in productIds:
 				if productProblemsOnDepot.has_key(productId):
 					for clientId in productProblemsOnDepot[productId]:
 						message += u"%s (%s) " % (clientId, productId)
 	
-		elif productVersionProblemsOnClient:
+		if productVersionProblemsOnClient:
 			message += u"Version of Installed Software on Client differs from Software on Depot: "
 			for productId in productIds:
 				if productVersionProblemsOnClient.has_key(productId):
 					for clientId in productVersionProblemsOnClient[productId]:
 						message += u"%s (%s) " % (clientId, productId)
 		
-		else:
+		if not productProblemsOnClient and not productProblemsOnDepot and not productVersionProblemsOnClient:
 			message += u"Checked Product looks good, no problems found."
 		print ">>>>>>>>>>>>>>>",state
 		print ">>>>>>>>>>>>>>>",message
@@ -335,5 +549,5 @@ class Monitoring(object):
 	
 	def checkPluginOnClient(self, clientId, plugin, params=None, state=None):
 		pass
-
+'''
 

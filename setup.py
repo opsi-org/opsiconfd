2afcd1fc7f0151f17921c9027061b4c4bcc37105
Niko Wenselowski <n.wenselowski@uib.de>
2017-04-26 14:12:20 +0200

    Correct comment.

--- setup.py
+++ setup.py
@@ -37,8 +37,8 @@ with codecs.open(os.path.join("debian", "changelog"), 'r', 'utf-8') as changelog
 if not VERSION:
 	raise Exception(u"Failed to get version info")
 
-# Always set __version__ in OPSI.__init__.py to the version found in
-# the changelog to make sure the version is always up-to-date
+# Always set __version__ in opsiconfd.__init__.py to the version found
+# in the changelog to make sure the version is always up-to-date
 # and nobody needs to manually update it.
 initFilePath = os.path.join('opsiconfd', '__init__.py')
 newInitLines = []

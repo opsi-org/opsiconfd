875f41292a93d8949e09764d8d2e6780305cb752
Niko Wenselowski <n.wenselowski@uib.de>
2019-07-05 16:00:37 +0200

    These files have been removed.

--- create_source.sh
+++ create_source.sh
@@ -28,7 +28,7 @@ cp rpm/${packagename}.spec $destdir/
 
 test -e $tmpdir && rm -rf $tmpdir
 mkdir $tmpdir
-cp -r debian setup.py opsiconfd data scripts ${tmpdir}/
+cp -r debian data ${tmpdir}/
 find ${tmpdir} -iname "*.pyc"   -exec rm "{}" \;
 find ${tmpdir} -iname "*.marks" -exec rm "{}" \;
 find ${tmpdir} -iname "*~"      -exec rm "{}" \;

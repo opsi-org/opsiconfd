a35c2e34b234206dd3a7bfc3c88cc8a490bddaf6
Jan Schneider <j.schneider@uib.de>
2020-04-24 16:12:21 +0200

    configure artifacts

--- opsiconfd/application/jsonrpc.py
+++ opsiconfd/application/jsonrpc.py
@@ -102,7 +102,7 @@ async def process_jsonrpc(request: Request, response: Response):
 		if body:
 			jsonrpc = body
 			if "gzip" in request.headers.get("content-encoding", ""):
-				jsonrpc = await self.loop.run_in_executor(None, lambda: ogzip.decompress(jsonrpc))
+				jsonrpc = await run_in_threadpool(gzip.decompress, jsonrpc)
 		else:
 			jsonrpc = urllib.parse.unquote(request.url.query)
 		logger.trace("jsonrpc: %s", jsonrpc)

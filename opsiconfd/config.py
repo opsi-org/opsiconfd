2597b2b391545e4e1474423a2676400302506bff
Jan Schneider <j.schneider@uib.de>
2020-06-04 18:00:38 +0200

    Correct log format

--- opsiconfd/config.py
+++ opsiconfd/config.py
@@ -224,7 +224,7 @@ parser.add(
 parser.add(
 	"--log-format-file",
 	env_var="OPSICONFD_LOG_FORMAT_FILE",
-	default="[%(opsilevel)s] [%(asctime)s.%(msecs)03d] %(message)s   (%(filename)s:%(lineno)d)",
+	default="[%(opsilevel)d] [%(asctime)s.%(msecs)03d] %(message)s   (%(filename)s:%(lineno)d)",
 	help="Set the log format for logfiles."
 )
 parser.add(
@@ -240,7 +240,7 @@ parser.add(
 parser.add(
 	"--log-format-stderr",
 	env_var="OPSICONFD_LOG_FORMAT_STDERR",
-	default="%(log_color)s[%(opsilevel)s] [%(asctime)s.%(msecs)03d]%(reset)s [%(client_address)s] %(message)s   (%(filename)s:%(lineno)d)",
+	default="%(log_color)s[%(opsilevel)d] [%(asctime)s.%(msecs)03d]%(reset)s [%(client_address)s] %(message)s   (%(filename)s:%(lineno)d)",
 	help="Set the log format for stder."
 )
 #parser.add(

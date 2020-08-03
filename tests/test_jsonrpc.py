b28fef871746d7643597489e22ec80e1df1227f4
Fabian Kalweit <f.kalweit@uib.de>
2020-08-03 12:53:03 +0200

    [new] jsonrpc test with request

---
+++ tests/test_jsonrpc.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import pytest
+import asyncio
+import time
+import urllib3
+import redis
+import aredis
+import requests
+import json
+
+OPSI_URL = "https://localhost:4447" 
+TEST_USER = "adminuser"
+TEST_PW = "adminuser"
+OPSI_SESSION_KEY = "opsiconfd:sessions"
+
+
+@pytest.fixture(autouse=True)
+@pytest.mark.asyncio
+async def clean_redis():
+	yield None
+	redis_client = aredis.StrictRedis.from_url("redis://redis")
+	session_keys = redis_client.scan_iter(f"{OPSI_SESSION_KEY}:127.0.0.1:*")
+	async for key in session_keys:
+		await redis_client.delete(key)
+	await redis_client.delete("opsiconfd:stats:client:failed_auth:127.0.0.1")
+	await redis_client.delete("opsiconfd:stats:client:blocked:127.0.0.1")
+	session_keys = redis_client.scan_iter("opsiconfd:stats:rpc:*")
+	async for key in session_keys:
+		print(key)
+		await redis_client.delete(key)
+	await redis_client.delete("opsiconfd:stats:num_rpcs")
+
+
+@pytest.fixture(autouse=True)
+def disable_request_warning():
+	urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def test_process_jsonrpc():
+	data = {"id": 1, "method": "host_getIdents", "params": [None]}
+	rpc_request_data = json.dumps(data)
+	r = requests.post(f"{OPSI_URL}/rpc", auth=(TEST_USER, TEST_PW), data=rpc_request_data, verify=False)
+	result_json = json.loads(r.text)
+
+	print(result_json)
+
+	assert r.status_code == 200
+	assert len(result_json.get("result")) == 823
+	assert result_json.get("result")[0] == "192.168.26.40"
+	assert result_json.get("method") == "host_getIdents"

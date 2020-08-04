9406d96917f5493b11a13f5291f37e7d4e66a938
Fabian Kalweit <f.kalweit@uib.de>
2020-08-04 17:16:43 +0200

    [chg] run all local tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker exec -it docker_opsiconfd_1 poetry run pytest --cov opsiconfd /src/tests/test_statistics.py  
+docker exec -it docker_opsiconfd_1 poetry run pytest --cov opsiconfd /src/tests
 #-k 'test_get_rpc_list_request' #'test_admin_interface_index' #>> test
 # docker exec -it docker_opsiconfd_1 poetry run pytest -r a --cov opsiconfd /src/tests
 # docker exec -it docker_opsiconfd_1 poetry run pytest --disable-warnings /src/tests

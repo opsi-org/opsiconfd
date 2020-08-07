9c98cfdcb04cd06d125694bb8b1ebbbd4b097e45
Fabian Kalweit <f.kalweit@uib.de>
2020-08-07 15:26:47 +0200

    [chg] removed deprecated yield_fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 import asyncio
 
-@pytest.yield_fixture(scope='session')
+@pytest.fixture(scope='session')
 def event_loop(request):
 	"""Create an instance of the default event loop for each test case."""
 	loop = asyncio.get_event_loop_policy().new_event_loop()

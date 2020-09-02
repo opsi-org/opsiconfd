f16485d6e26ec83c95486d1955d11542f31fe912
Fabian Kalweit <f.kalweit@uib.de>
2020-09-02 17:12:25 +0200

    [chg] clean up

--- opsiconfd_data/static/javascript/admininterface.js
+++ opsiconfd_data/static/javascript/admininterface.js
@@ -383,7 +383,6 @@ function callJSONRPC() {
 			result = request.responseText
 			result = JSON.parse(result);
 			outputToHTML(result, "jsonrpc-result");
-			// loadRPCTable("rpc_num", false);
 			return result;
 
 		} else {

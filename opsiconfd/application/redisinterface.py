73f7b7fa57262d6194490a909e7678e3978dd273
Fabian Kalweit <f.kalweit@uib.de>
2020-09-24 12:34:41 +0200

    [new] redis stats endpoint

--- opsiconfd/application/redisinterface.py
+++ opsiconfd/application/redisinterface.py
@@ -50,3 +50,73 @@ async def redis_command(request: Request, response: Response):
 			error["details"] = str(tb)
 		response = JSONResponse({"status": 500, "error": error, "data": {"result": None} })
 	return response
+
+
+
+@admin_interface_router.get("/redis-stats")
+async def get_redis_stats():
+	redis_client = await get_redis_client()
+	try: 
+		
+		stats_keys = []
+		sessions_keys = []
+		log_keys = []
+		misc_keys = []
+
+		redis_keys = redis_client.scan_iter("opsiconfd:*")
+		
+		async for key in redis_keys:
+			key = key.decode("utf8")
+			if key.startswith("opsiconfd:stats"):
+				stats_keys.append(key)
+			elif key.startswith("opsiconfd:sessions"):
+				sessions_keys.append(key)
+			elif key.startswith("opsiconfd:log"):
+				log_keys.append(key)
+			else:
+				logger.devel(key)
+				misc_keys.append(key)
+		
+		stats_memory = 0
+		for key in stats_keys:
+			stats_memory += await redis_client.execute_command(f"MEMORY USAGE {key}")
+			
+		sessions_memory = 0
+		for key in sessions_keys:
+			sessions_memory += await redis_client.execute_command(f"MEMORY USAGE {key}")
+
+		logs_memory = 0
+		for key in log_keys:
+			logs_memory += await redis_client.execute_command(f"MEMORY USAGE {key}")
+		
+		misc_memory = 0
+		for key in misc_keys:
+			misc_memory += await redis_client.execute_command(f"MEMORY USAGE {key}")
+		
+		keys = {
+			"stats":{
+				"count":len(stats_keys),
+				"memory": stats_memory
+			},
+			"sessions":{
+				"count":len(sessions_keys),
+				"memory": sessions_memory
+			},
+			"logs":{
+				"count":len(log_keys),
+				"memory": logs_memory
+			},
+			"misc":{
+				"count":len(misc_keys),
+				"memory": misc_memory
+			}
+		}
+		#.format(len(stats_keys), len(sessions_keys),len(log_keys),len(misc_keys))
+		
+		logger.devel(keys)
+		response = JSONResponse({"status": 200, "error": None, "data": {"keys": keys}})
+		logger.devel(response.__dict__)
+	except Exception as e:
+		logger.error("Error while reading redis data: %s", e)
+		response = JSONResponse({"status": 500, "error": { "message": "Error while reading redis data", "detail": str(e)}})
+	return response

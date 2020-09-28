3848b9adeaec6b10c2809e2e4302acccaf8bc784
Fabian Kalweit <f.kalweit@uib.de>
2020-09-28 09:28:26 +0200

    [chg] rm some test skips

--- tests/test_admininterface.py
+++ tests/test_admininterface.py
@@ -94,7 +94,6 @@ async def test_unblock_all_request():
 	r = requests.get(OPSI_URL, auth=(TEST_USER, TEST_PW), verify=False)
 	assert r.status_code == 200
 
-@pytest.mark.skip(reason="test does not work in gitlab ci")
 @pytest.mark.asyncio
 async def test_unblock_client_request():
 	admin_request = requests.get(f"{OPSI_URL}/admin", auth=(TEST_USER, TEST_PW), verify=False)
@@ -105,7 +104,6 @@ async def test_unblock_client_request():
 	r = requests.get(OPSI_URL, auth=(TEST_USER, TEST_PW), verify=False)
 	assert r.status_code == 200
 
-@pytest.mark.skip(reason="test does not work in gitlab ci")
 def test_get_rpc_list_request():
 	for i in range(0, 3):
 		call_rpc([{"id": 1, "method": "host_getIdents","params": [None]}], [False])
@@ -131,7 +129,6 @@ async def test_get_blocked_clients_request():
 
 
 get_rpc_list_test_data = [1,3,5]
-@pytest.mark.skip(reason="test does not work in gitlab ci")
 @pytest.mark.parametrize("num_rpcs", get_rpc_list_test_data)
 @pytest.mark.asyncio
 async def test_get_rpc_list(admininterface, num_rpcs):
@@ -210,7 +207,7 @@ async def test_delete_client_sessions(admininterface, rpc_request_data, expected
 		keys.append(key)
 	assert len(keys) == expected_key_len
 
-@pytest.mark.skip(reason="test does not work in gitlab ci")
+
 @pytest.mark.asyncio
 async def test_unblock_all(admininterface):
 	headers = Headers()
@@ -239,7 +236,7 @@ async def test_unblock_all(admininterface):
 	r = requests.get(OPSI_URL, auth=(TEST_USER, TEST_PW), verify=False)
 	assert r.status_code == 200
 	
-@pytest.mark.skip(reason="test does not work in gitlab ci")
+
 @pytest.mark.asyncio
 async def test_unblock_client(admininterface):
 

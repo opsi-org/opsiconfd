c3c9eefcc0b04209e85a64fbc1cef13498de9cd2
Fabian Kalweit <f.kalweit@uib.de>
2020-11-06 17:18:27 +0100

    [chg] start all tests with run_tests.py

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -it docker_opsiconfd_1 poetry run pytest --cov -s opsiconfd /src/tests/test_productOrdering.py
+docker exec -it docker_opsiconfd_1 poetry run pytest -v -s opsiconfd /src/tests

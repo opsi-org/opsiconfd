f475364f67725bfae1e94915d28a4fc2aacb58fd
Jan Schneider <j.schneider@uib.de>
2020-11-16 15:54:22 +0100

    [fix] fix timezone

--- opsiconfd_data/static/javascript/log_viewer.js
+++ opsiconfd_data/static/javascript/log_viewer.js
@@ -12,7 +12,7 @@ function addRecordToLog(record) {
 	logLineId++;
 
 	//console.log(record)
-	let date = new Date((record.created - (new Date()).getTimezoneOffset() * 60) * 1000);
+	let date = new Date(record.created * 1000);
 	let day = String(date.getDate()).padStart(2, "0");
 	let month = String(date.getMonth() + 1).padStart(2, "0");
 	let year = String(date.getFullYear());

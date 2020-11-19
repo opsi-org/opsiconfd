5cefda2d4d1800557357b1c1f25051a973af0a84
Fabian Kalweit <f.kalweit@uib.de>
2020-11-19 12:16:57 +0100

    [chg] mv mysql in opsiconfd dev container

--- docker/files/docker-entrypoint.sh
+++ docker/files/docker-entrypoint.sh
@@ -6,28 +6,28 @@ set -o nounset
 # cmd="$*"
 cmd="sleep infinity"
 
-mysql_ready () {
-	# Check that mysql is up and running:
-	dockerize -wait 'tcp://mysql:3306' -timeout 5s
-}
+# mysql_ready () {
+# 	# Check that mysql is up and running:
+# 	dockerize -wait 'tcp://mysql:3306' -timeout 5s
+# }
 
 redis_ready () {
 	# Check that redis is up and running:
 	dockerize -wait 'tcp://redis:6379' -timeout 5s
 }
 
-until mysql_ready; do
-	echo 'Waiting for MySQL...' 1>&2
-done
-echo 'MySQL is ready.' 1>&2
+# until mysql_ready; do
+# 	echo 'Waiting for MySQL...' 1>&2
+# done
+# echo 'MySQL is ready.' 1>&2
 
 until redis_ready; do
 	echo 'Waiting for Redis...' 1>&2
 done
 echo 'Redis is ready.' 1>&2
 
-echo 'Restore opsi database' 1>&2
-zcat /opsi.sql.gz | mariadb -h mysql -u opsi --password=opsi opsi
+# echo 'Restore opsi database' 1>&2
+# zcat /opsi.sql.gz | mariadb -h mysql -u opsi --password=opsi opsi
 
 echo 'Set depotserver name in /etc/hosts' 1>&2
 echo "127.0.0.1       localhost bonifax.uib.local" >/tmp/hosts

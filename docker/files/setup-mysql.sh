b4422119b55282f08a26a908dcdecb00e59083c0
Fabian Kalweit <f.kalweit@uib.de>
2020-12-03 15:39:48 +0100

    [chg] dont activate venv / clean up

--- docker/files/setup-mysql.sh
+++ docker/files/setup-mysql.sh
@@ -8,38 +8,8 @@ echo $MYSQL_PASSWORD
 
 service mysql start
 
-# apt-get -y install expect
-
-# SECURE_MYSQL=$(expect -c "
-# set timeout 10
-# spawn mysql_secure_installation
-# expect \"Enter current password for root (enter for none):\"
-# send \"$MYSQL_OLD_ROOT_PASSWORD\r\"
-# expect \"Change the root password?\"
-# send \"y\r\"
-# expect \"New password:\"
-# send \"$MYSQL_ROOT_PASSWORD\r\"
-# expect \"Re-enter new password:\"
-# send \"$MYSQL_ROOT_PASSWORD\r\"
-# expect \"Remove anonymous users?\"
-# send \"y\r\"
-# expect \"Disallow root login remotely?\"
-# send \"y\r\"
-# expect \"Remove test database and access to it?\"
-# send \"y\r\"
-# expect \"Reload privilege tables now?\"
-# send \"y\r\"
-# expect eof
-# ")
-
-# echo "$SECURE_MYSQL"
-
-# apt-get -y purge expect
-
 echo "set mysql root pw"
 sudo mysqladmin -u root --password=$MYSQL_OLD_ROOT_PASSWORD password $MYSQL_ROOT_PASSWORD
-mysql -u root --password=$MYSQL_ROOT_PASSWORD -e "UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root'"
-# mysql -u root --password=$MYSQL_ROOT_PASSWORD -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
 mysql -u root --password=$MYSQL_ROOT_PASSWORD -e "DELETE FROM mysql.user WHERE User=''"
 echo "remove test db"
 mysql -u root --password=$MYSQL_ROOT_PASSWORD -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"

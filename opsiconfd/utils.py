33a8d180d9f7d65b8dfc157eaa152b968d5f01b3
Jan Schneider <j.schneider@uib.de>
2021-01-16 13:37:05 +0100

    [new,pub] Publish opsi config service with zeroconf

--- opsiconfd/utils.py
+++ opsiconfd/utils.py
@@ -171,10 +171,18 @@ def get_ip_addresses():
 				family = "ipv6"
 			else:
 				continue
+
+			ip_address = None
+			try:
+				ip_address = ipaddress.ip_address(snic.address.split('%')[0])
+			except ValueError:
+				logger.warning("Unrecognised ip address: %r", snic.address)
+
 			yield {
 				"family": family,
 				"interface": interface,
-				"address": snic.address
+				"address": snic.address,
+				"ip_address": ip_address
 			}
 
 def get_fqdn(name=''):

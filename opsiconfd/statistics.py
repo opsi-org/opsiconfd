9a9943f6090890be5e9a9ebf1fdf7835e9b4950f
Jan Schneider <j.schneider@uib.de>
2021-01-31 13:30:00 +0100

    [chg] Add zero one interval before

--- opsiconfd/statistics.py
+++ opsiconfd/statistics.py
@@ -454,11 +454,11 @@ class MetricsCollector(): #  pylint: disable=too-many-instance-attributes
 						for key_string in list(self._values.get(metric.id, {})):
 							value = 0
 							count = 0
-							insert_one_zero = False
+							insert_zero_timestamp = 0
 							for tsp in list(self._values[metric.id].get(key_string, {})):
 								if self._values[metric.id][key_string][tsp] is None:
 									# Marker, insert a zero before adding new values
-									insert_one_zero = True
+									insert_zero_timestamp = tsp
 									self._values[metric.id][key_string].pop(tsp)
 									continue
 								if tsp <= timestamp:
@@ -468,7 +468,7 @@ class MetricsCollector(): #  pylint: disable=too-many-instance-attributes
 							if count == 0:
 								if not metric.zero_if_missing:
 									continue
-								if not insert_one_zero and metric.zero_if_missing == "one":
+								if not insert_zero_timestamp and metric.zero_if_missing == "one":
 									del self._values[metric.id][key_string]
 
 							if metric.aggregation == "avg" and count > 0:
@@ -479,9 +479,9 @@ class MetricsCollector(): #  pylint: disable=too-many-instance-attributes
 							for idx, var in enumerate(metric.vars):
 								labels[var] = label_values[idx]
 
-							if insert_one_zero:
+							if insert_zero_timestamp:
 								cmds.append(
-									self._redis_ts_cmd(metric, "ADD", 0, timestamp-1001, **labels)
+									self._redis_ts_cmd(metric, "ADD", 0, insert_zero_timestamp, **labels)
 								)
 
 							cmd = self._redis_ts_cmd(metric, "ADD", value, timestamp, **labels)
@@ -566,7 +566,7 @@ class MetricsCollector(): #  pylint: disable=too-many-instance-attributes
 					# Insert a zero before new adding new values because
 					# gaps in diagrams will be conneced with straight lines.
 					# Marking with None
-					self._values[metric_id][key_string][timestamp-1001] = None
+					self._values[metric_id][key_string][timestamp-self._interval*1000] = None
 			if not timestamp in self._values[metric_id][key_string]:
 				self._values[metric_id][key_string][timestamp] = 0
 			self._values[metric_id][key_string][timestamp] += value

bcc266d7359f6a51ba6ec11c6c3d4b8deb8510c6
Fabian Kalweit <f.kalweit@uib.de>
2021-02-11 11:50:47 +0100

    [chg] devcontainer: install jemalloc / debug option jemalloc

--- scripts/devenv.sh
+++ scripts/devenv.sh
@@ -2,4 +2,11 @@
 echo OPSICONFD_GRAFANA_EXTERNAL_URL=http://$(hostname -f):3000 > .env
 echo OPSI_HOSTNAME=$(hostname -f) >> .env
 echo OPSI_DOMAIN=$(hostname -d) >> .env
-echo DEV_USER=$USER >> .env
+
+if [ -z ${USER+x} ]; then
+	echo DEV_USER=$DEV_USER >> .env
+else
+	echo DEV_USER=$USER >> .env
+fi
+
+echo JEMALLOC_VERSION=5.2.1 >> .env

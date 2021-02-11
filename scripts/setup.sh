ce8423686dadcb23d1d3b7433ea5ab1a5975b89d
Fabian Kalweit <f.kalweit@uib.de>
2021-02-11 11:50:47 +0100

    [chg] devcontainer: install jemalloc / debug option jemalloc

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,6 +11,18 @@ rm /tmp/hosts
 /workspace/scripts/setup-grafana.sh
 /workspace/scripts/setup-mysql.sh
 
+if [ -n "$JEMALLOC_VERSION" ]; then
+	# installing jemalloc
+	echo "installing jemalloc"
+	cd /tmp
+	wget https://github.com/jemalloc/jemalloc/releases/download/$JEMALLOC_VERSION/jemalloc-$JEMALLOC_VERSION.tar.bz2
+	tar xvjf jemalloc-$JEMALLOC_VERSION.tar.bz2
+	cd jemalloc-$JEMALLOC_VERSION
+	./configure
+	make
+	sudo make install
+fi
+
 mkdir /var/log/opsi
 mkdir /var/lib/opsi/workbench
 # sudo opsi-backup restore /workspace/opsi_dev_backup.tar.bz2

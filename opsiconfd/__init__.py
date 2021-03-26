2ab528430374b79c41b77cc70ca825f7c8ad76a9
Jan Schneider <j.schneider@uib.de>
2021-03-26 13:39:18 +0100

    [chg] Skip link local address

--- opsiconfd/__init__.py
+++ opsiconfd/__init__.py
@@ -22,7 +22,7 @@ The opsi configuration service.
 :author: Jan Schneider <j.schneider@uib.de>
 :license: GNU Affero General Public License version 3
 """
-__version__ = '4.2.0.146'
+__version__ = '4.2.0.149'
 
 import contextvars
 

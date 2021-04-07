38cbb55d3a86078aa6d855472ffc0db30e9b833b
Fabian Kalweit <f.kalweit@uib.de>
2021-04-07 15:57:20 +0000

    [chg] add tests check_locked_products / fix tests check_disk_usage

--- tests/test_monitoring.py
+++ tests/test_monitoring.py
@@ -12,12 +12,18 @@ Tests for the opsiconfd monitoring module
 import errno
 import os
 import socket
+import time
+import tempfile
 import unittest.mock as mock
 import json
 import pytest
-from fastapi.responses import JSONResponse
+
+import MySQLdb
 
 from opsiconfd.application.monitoring.check_opsi_disk_usage import check_opsi_disk_usage
+from opsiconfd.application.monitoring.check_locked_products import check_locked_products
+from opsiconfd.backend import get_backend
+from .utils import clean_redis, config, create_check_data, TEST_USER, TEST_PW, HOSTNAME, LOCAL_IP, DAYS # pylint: disable=unused-import
 
 test_data = [
 	(
@@ -27,9 +33,9 @@ test_data = [
 			'used': 85899345920,
 			'usage': 0.80
 		},
-		"/var/opsi",
+		"/var/log/opsi",
 		{},
-		{"state": 0, "message": "OK: DiskUsage from ressource '/var/opsi' is ok. (available:  20.00GB)."}
+		{"state": 0, "message": "OK: DiskUsage from ressource '/var/log/opsi' is ok. (available:  20.00GB)."}
 	),
 	(
 		{
@@ -38,9 +44,9 @@ test_data = [
 			'used': 106300440576,
 			'usage': 0.99
 		},
-		"/var/opsi",
+		"/var/log/opsi",
 		{},
-		{"state": 2, "message": "CRITICAL: DiskUsage from ressource: '/var/opsi' is critical (available: 1.00GB)."}
+		{"state": 2, "message": "CRITICAL: DiskUsage from ressource: '/var/log/opsi' is critical (available: 1.00GB)."}
 	),
 	(
 		{
@@ -49,9 +55,9 @@ test_data = [
 			'used': 102005473280,
 			'usage': 0.95
 		},
-		"/var/opsi",
+		"/var/log/opsi",
 		{},
-		{"state": 1, "message": "WARNING: DiskUsage warning from ressource: '/var/opsi' (available: 5.00GB)."}
+		{"state": 1, "message": "WARNING: DiskUsage warning from ressource: '/var/log/opsi' (available: 5.00GB)."}
 	),
 	(
 		{
@@ -60,9 +66,9 @@ test_data = [
 			'used': 85899345920,
 			'usage': 0.80
 		},
-		"/var/opsi",
+		"/var/log/opsi",
 		{"warning": "30G", "critical": "10G"},
-		{"state": 1, "message": "WARNING: DiskUsage warning from ressource: '/var/opsi' (available: 20.00GB)."}
+		{"state": 1, "message": "WARNING: DiskUsage warning from ressource: '/var/log/opsi' (available: 20.00GB)."}
 	),
 	(
 		{
@@ -71,9 +77,9 @@ test_data = [
 			'used': 85899345920,
 			'usage': 0.80
 		},
-		"/var/opsi",
+		"/var/log/opsi",
 		{"warning": "30G", "critical": "20G"},
-		{"state": 2, "message": "CRITICAL: DiskUsage from ressource: '/var/opsi' is critical (available: 20.00GB)."}
+		{"state": 2, "message": "CRITICAL: DiskUsage from ressource: '/var/log/opsi' is critical (available: 20.00GB)."}
 	),
 	(
 		{
@@ -82,10 +88,10 @@ test_data = [
 			'used': 85899345920,
 			'usage': 0.80
 		},
-		["/var/opsi", "/var/log/opsi"],
+		["/var/log/opsi", "/etc/opsi"],
 		{"warning": "30G", "critical": "20G"},
-		{"state": 2, "message": ("CRITICAL: DiskUsage from ressource: '/var/opsi' is critical (available: 20.00GB). "
-		"DiskUsage from ressource: '/var/log/opsi' is critical (available: 20.00GB).")}
+		{"state": 2, "message": ("CRITICAL: DiskUsage from ressource: '/var/log/opsi' is critical (available: 20.00GB). "
+		"DiskUsage from ressource: '/etc/opsi' is critical (available: 20.00GB).")}
 	),
 	(
 		{
@@ -94,10 +100,10 @@ test_data = [
 			'used': 85899345920,
 			'usage': 0.80
 		},
-		["/var/opsi", "/var/log/opsi"],
+		["/var/log/opsi", "/etc/opsi"],
 		{"warning": "30%", "critical": "20%"},
-		{"state": 2, "message": ("CRITICAL: DiskUsage from ressource: '/var/opsi' is critical (available: 20.00%). "
-		"DiskUsage from ressource: '/var/log/opsi' is critical (available: 20.00%).")}
+		{"state": 2, "message": ("CRITICAL: DiskUsage from ressource: '/var/log/opsi' is critical (available: 20.00%). "
+		"DiskUsage from ressource: '/etc/opsi' is critical (available: 20.00%).")}
 	),
 	(
 		{
@@ -106,9 +112,9 @@ test_data = [
 			'used': 85899345920,
 			'usage': 0.80
 		},
-		"/var/opsi",
+		"/var/log/opsi",
 		{"warning": "10%", "critical": "5%"},
-		{"state": 0, "message": "OK: DiskUsage from ressource: '/var/opsi' is ok. (available: 20.00%)."}
+		{"state": 0, "message": "OK: DiskUsage from ressource: '/var/log/opsi' is ok. (available: 20.00%)."}
 	),
 	(
 		{
@@ -119,7 +125,7 @@ test_data = [
 		},
 		"var/log/opsi",
 		{"warning": "10%", "critical": "5%"},
-		{"state": 0, "message": "OK: DiskUsage from ressource: '/var/log/opsi' is ok. (available: 20.00%)."}
+		{"state": 3, "message": "UNKNOWN: No results get. Nothing to check."}
 	)
 
 ]
@@ -130,6 +136,9 @@ def test_check_disk_usage(info, opsiresource, thresholds, expected_result): # py
 	def get_info(path):
 		print(path)
 		return info
+
+
+	# with tempfile.mkdtemp(dir = "c:/python36"):
 
 	with mock.patch('opsiconfd.application.monitoring.check_opsi_disk_usage.getDiskSpaceUsage', get_info):
 		result = check_opsi_disk_usage(thresholds=thresholds, opsiresource=opsiresource)
@@ -143,11 +152,11 @@ def test_check_disk_usage_error(): # pylint: disable=too-many-arguments
 		raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path)
 
 	with mock.patch('opsiconfd.application.monitoring.check_opsi_disk_usage.getDiskSpaceUsage', get_info):
-		result = check_opsi_disk_usage(opsiresource="/file/not/found")
+		result = check_opsi_disk_usage(thresholds={}, opsiresource="/etc/opsi")
 
 	assert json.loads(result.body) == {
 		'message': ('UNKNOWN: ["Not able to check DiskUsage. Error: \'[Errno 2] '
-			'No such file or directory: \'/workspace/opsiconfd_data/static\'\'"]'),
+			'No such file or directory: \'/etc/opsi\'\'"]'),
 		'state': 3
 		}
 
@@ -171,3 +180,35 @@ def test_check_disk_usage_no_result(return_value): # pylint: disable=too-many-ar
 		'message': ('UNKNOWN: No results get. Nothing to check.'),
 		'state': 3
 		}
+
+
+def test_check_locked_products():
+
+	mysql_host = os.environ.get("MYSQL_HOST")
+	if not mysql_host:
+		mysql_host = "127.0.0.1"
+
+	db=MySQLdb.connect(host=mysql_host,user="opsi",passwd="opsi",db="opsi") # pylint: disable=invalid-name, c-extension-no-member
+	db.autocommit(True)
+	cursor = db.cursor()
+	cursor.execute((
+			'REPLACE INTO PRODUCT_ON_DEPOT (productId, productVersion, packageVersion, depotId, productType, locked) '
+			'VALUES ("pytest-prod-3", "1.0", "1", "pytest-test-depot.uib.gmbh", "LocalbootProduct", true);'
+			'REPLACE INTO PRODUCT_ON_DEPOT (productId, productVersion, packageVersion, depotId, productType, locked) '
+			'VALUES ("pytest-prod-2", "1.0", "1", "pytest-test-depot.uib.gmbh", "LocalbootProduct", true);'
+		)
+	)
+	cursor.close()
+
+	time.sleep(2)
+
+	backend = get_backend()
+
+	result = check_locked_products(backend, depot_ids=["pytest-test-depot.uib.gmbh"])
+	assert json.loads(result.body) == {'message': 'WARNING: 2 products are in locked state.\nProduct pytest-prod-2 locked on depot pytest-test-depot.uib.gmbh\nProduct pytest-prod-3 locked on depot pytest-test-depot.uib.gmbh', 'state': 1}
+
+	result = check_locked_products(backend, depot_ids=["pytest-test-depot.uib.gmbh", socket.getfqdn()])
+	assert json.loads(result.body) == {'message': 'WARNING: 2 products are in locked state.\nProduct pytest-prod-2 locked on depot pytest-test-depot.uib.gmbh\nProduct pytest-prod-3 locked on depot pytest-test-depot.uib.gmbh', 'state': 1}
+
+	result = check_locked_products(backend, depot_ids=["pytest-test-depot.uib.gmbh", socket.getfqdn()], product_ids=["pytest-prod-2"])
+	assert json.loads(result.body) == {'message': 'WARNING: 1 products are in locked state.\nProduct pytest-prod-2 locked on depot pytest-test-depot.uib.gmbh', 'state': 1}
--- tests/utils.py
+++ tests/utils.py
@@ -5,19 +5,25 @@
 # All rights reserved.
 # License: AGPL-3.0
 
+import os
 import asyncio
 import pytest
 import socket
+import json
+from datetime import datetime, timedelta
 import aredis
-
+import urllib3
+import requests
 import sys
 
+import MySQLdb
 
 TEST_USER = "adminuser"
 TEST_PW = "adminuser"
 HOSTNAME = socket.gethostname()
 LOCAL_IP = socket.gethostbyname(HOSTNAME)
 OPSI_SESSION_KEY = "opsiconfd:sessions"
+DAYS = 31
 
 @pytest.fixture(name="config")
 def config(monkeypatch):
@@ -44,3 +50,125 @@ async def clean_redis(config): # pylint: disable=redefined-outer-name
 	async for key in rpc_keys:
 		await redis_client.delete(key)
 	await asyncio.sleep(5)
+
+
+
+def create_depot(opsi_url, depot_name):
+	params= [depot_name,None,"file:///var/lib/opsi/depot","smb://172.17.0.101/opsi_depot",None,"file:///var/lib/opsi/repository","webdavs://172.17.0.101:4447/repository"] # pylint: disable=line-too-long
+
+	rpc_request_data = json.dumps({"id": 1, "method": "host_createOpsiDepotserver", "params": params})
+	res = requests.post(f"{opsi_url}/rpc", auth=(TEST_USER, TEST_PW), data=rpc_request_data, verify=False)
+	result_json = json.loads(res.text)
+	print(result_json)
+
+
+
+@pytest.fixture(autouse=True)
+def create_check_data(config):
+
+	mysql_host = os.environ.get("MYSQL_HOST")
+	if not mysql_host:
+		mysql_host = "127.0.0.1"
+
+	db=MySQLdb.connect(host=mysql_host,user="opsi",passwd="opsi",db="opsi") # pylint: disable=invalid-name, c-extension-no-member
+	now = datetime.now()
+
+	db.autocommit(True)
+	cursor = db.cursor()
+
+	cursor.execute(
+		(
+			'DELETE FROM PRODUCT_ON_DEPOT WHERE productId like "pytest%";'
+			'DELETE FROM PRODUCT_ON_CLIENT WHERE productId like "pytest%";'
+			'DELETE FROM PRODUCT WHERE productId like "pytest%";'
+			'DELETE FROM HOST WHERE hostId like "pytest%";'
+		)
+	)
+
+	# Product
+	for i in range(0,5):
+		sql_string = (f'INSERT INTO HOST (hostId, type, created, lastSeen) VALUES ("pytest-client-{i}.uib.local", '
+			f'"OpsiClient", "{now}", "{now}");')
+		cursor.execute(sql_string)
+		sql_string = ('INSERT INTO PRODUCT (productId, productVersion, packageVersion, type,  name, priority) '
+			f'VALUES ("pytest-prod-{i}", "1.0", "1", "LocalbootProduct", "Pytest dummy PRODUCT {i}", 60+{i});')  # pylint: disable=line-too-long
+		cursor.execute(sql_string)
+		sql_string = f'INSERT INTO PRODUCT_ON_DEPOT (productId, productVersion, packageVersion, depotId, productType) VALUES ("pytest-prod-{i}", "1.0", "1", "{socket.getfqdn()}", "LocalbootProduct");' # pylint: disable=line-too-long
+		cursor.execute(sql_string)
+
+	cursor.execute(
+		(
+			'INSERT INTO PRODUCT (productId, productVersion, packageVersion, type,  name, priority) '
+			'VALUES ("pytest-prod-1", "2.0", "1", "LocalbootProduct", "Pytest dummy PRODUCT 1 version 2", 60);'
+		)
+	)
+
+	# Host
+	cursor.execute(
+		(
+			'INSERT INTO HOST (hostId, type, created, lastSeen)'
+			f'VALUES ("pytest-lost-client.uib.local", "OpsiClient", "{now}", "{now-timedelta(days=DAYS)}");'
+			'INSERT INTO HOST (hostId, type, created, lastSeen) '
+			f'VALUES ("pytest-lost-client-fp.uib.local", "OpsiClient", "{now}", "{now-timedelta(days=DAYS)}");'
+			'INSERT INTO HOST (hostId, type, created, lastSeen) '
+			f'VALUES ("pytest-lost-client-fp2.uib.local", "OpsiClient", "{now}", "{now-timedelta(days=DAYS)}");'
+		)
+	)
+
+	create_depot(config.internal_url, "pytest-test-depot.uib.gmbh")
+	create_depot(config.internal_url, "pytest-test-depot2.uib.gmbh")
+
+	# Product on client
+	cursor.execute(
+		(
+			'INSERT INTO PRODUCT_ON_CLIENT '
+			'(productId, clientId, productType, installationStatus, actionRequest, actionResult, productVersion, packageVersion, modificationTime) '
+			f'VALUES ("pytest-prod-1", "pytest-client-1.uib.local", "LocalbootProduct", "not_installed", "setup", "none", "1.0", 1, "{now}");'
+			'INSERT INTO PRODUCT_ON_CLIENT '
+			'(productId, clientId, productType, installationStatus, actionRequest, actionResult, productVersion, packageVersion, modificationTime) '
+	 		f'VALUES ("pytest-prod-2", "pytest-client-2.uib.local", "LocalbootProduct", "unknown", "none", "failed", "1.0", 1, "{now}");'
+			'INSERT INTO PRODUCT_ON_CLIENT '
+			'(productId, clientId, productType, installationStatus, actionRequest, actionResult, productVersion, packageVersion, modificationTime) '
+	 		f'VALUES ("pytest-prod-3", "pytest-client-3.uib.local", "LocalbootProduct", "installed", "none", "none", "1.0", 1, "{now}");'
+			'INSERT INTO PRODUCT_ON_CLIENT '
+			'(productId, clientId, productType, installationStatus, actionRequest, actionResult, productVersion, packageVersion, modificationTime) '
+			f'VALUES ("pytest-prod-2", "pytest-lost-client-fp.uib.local", "LocalbootProduct", "unknown", "none", "failed", "1.0", 1, "{now}");'
+			'INSERT INTO PRODUCT_ON_CLIENT '
+			'(productId, clientId, productType, installationStatus, actionRequest, actionResult, productVersion, packageVersion, modificationTime) '
+			f'VALUES ("pytest-prod-2", "pytest-lost-client-fp2.uib.local", "LocalbootProduct", "unknown", "none", "failed", "1.0", 1, "{now}");'
+			'INSERT INTO PRODUCT_ON_CLIENT '
+			'(productId, clientId, productType, installationStatus, actionRequest, actionResult, productVersion, packageVersion, modificationTime) '
+			f'VALUES ("pytest-prod-1", "pytest-lost-client-fp2.uib.local", "LocalbootProduct", "not_installed", "setup", "none", "1.0", 1, "{now}");'
+		)
+	)
+
+	# Product on depot
+	cursor.execute((
+			'INSERT INTO PRODUCT_ON_DEPOT (productId, productVersion, packageVersion, depotId, productType) '
+			'VALUES ("pytest-prod-1", "1.0", "1", "pytest-test-depot.uib.gmbh", "LocalbootProduct");'
+			'INSERT INTO PRODUCT_ON_DEPOT (productId, productVersion, packageVersion, depotId, productType) '
+			'VALUES ("pytest-prod-2", "1.0", "1", "pytest-test-depot.uib.gmbh", "LocalbootProduct");'
+			'INSERT INTO PRODUCT_ON_DEPOT (productId, productVersion, packageVersion, depotId, productType) '
+			'VALUES ("pytest-prod-1", "2.0", "1", "pytest-test-depot2.uib.gmbh", "LocalbootProduct"); '
+			'INSERT INTO PRODUCT_ON_DEPOT (productId, productVersion, packageVersion, depotId, productType) '
+			'VALUES ("pytest-prod-2", "1.0", "1", "pytest-test-depot2.uib.gmbh", "LocalbootProduct");'
+		)
+	)
+	cursor.close()
+
+	yield
+
+	cursor = db.cursor()
+	cursor.execute(
+		(
+			'DELETE FROM PRODUCT_ON_DEPOT WHERE productId like "pytest%";'
+			'DELETE FROM PRODUCT_ON_CLIENT WHERE productId like "pytest%";'
+			'DELETE FROM PRODUCT WHERE productId like "pytest%";'
+			'DELETE FROM HOST WHERE hostId like "pytest%";'
+		)
+	)
+	cursor.close()
+
+@pytest.fixture(autouse=True)
+def disable_request_warning():
+	urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)

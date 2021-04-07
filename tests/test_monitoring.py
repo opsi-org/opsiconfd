e1f3107bfeb42d596667900b4a6abca062b2228a
Fabian Kalweit <f.kalweit@uib.de>
2021-04-07 14:27:16 +0000

    [chg] add tests / check if info is empty

--- tests/test_monitoring.py
+++ tests/test_monitoring.py
@@ -9,7 +9,8 @@
 Tests for the opsiconfd monitoring module
 '''
 
-
+import errno
+import os
 import socket
 import unittest.mock as mock
 import json
@@ -28,7 +29,7 @@ test_data = [
 		},
 		"/var/opsi",
 		{},
-		{"state": 0, "message": "OK: DiskUsage from ressource \'/var/opsi\' is ok. (available:  20.00GB)."}
+		{"state": 0, "message": "OK: DiskUsage from ressource '/var/opsi' is ok. (available:  20.00GB)."}
 	),
 	(
 		{
@@ -73,8 +74,7 @@ test_data = [
 		"/var/opsi",
 		{"warning": "30G", "critical": "20G"},
 		{"state": 2, "message": "CRITICAL: DiskUsage from ressource: '/var/opsi' is critical (available: 20.00GB)."}
-	)
-	,
+	),
 	(
 		{
 			'capacity': 107374182400,
@@ -86,7 +86,42 @@ test_data = [
 		{"warning": "30G", "critical": "20G"},
 		{"state": 2, "message": ("CRITICAL: DiskUsage from ressource: '/var/opsi' is critical (available: 20.00GB). "
 		"DiskUsage from ressource: '/var/log/opsi' is critical (available: 20.00GB).")}
+	),
+	(
+		{
+			'capacity': 107374182400,
+			'available': 21474836480,
+			'used': 85899345920,
+			'usage': 0.80
+		},
+		["/var/opsi", "/var/log/opsi"],
+		{"warning": "30%", "critical": "20%"},
+		{"state": 2, "message": ("CRITICAL: DiskUsage from ressource: '/var/opsi' is critical (available: 20.00%). "
+		"DiskUsage from ressource: '/var/log/opsi' is critical (available: 20.00%).")}
+	),
+	(
+		{
+			'capacity': 107374182400,
+			'available': 21474836480,
+			'used': 85899345920,
+			'usage': 0.80
+		},
+		"/var/opsi",
+		{"warning": "10%", "critical": "5%"},
+		{"state": 0, "message": "OK: DiskUsage from ressource: '/var/opsi' is ok. (available: 20.00%)."}
+	),
+	(
+		{
+			'capacity': 107374182400,
+			'available': 21474836480,
+			'used': 85899345920,
+			'usage': 0.80
+		},
+		"var/log/opsi",
+		{"warning": "10%", "critical": "5%"},
+		{"state": 0, "message": "OK: DiskUsage from ressource: '/var/log/opsi' is ok. (available: 20.00%)."}
 	)
+
 ]
 
 @pytest.mark.parametrize("info, opsiresource, thresholds, expected_result", test_data)
@@ -100,3 +135,39 @@ def test_check_disk_usage(info, opsiresource, thresholds, expected_result): # py
 		result = check_opsi_disk_usage(thresholds=thresholds, opsiresource=opsiresource)
 
 	assert json.loads(result.body) == expected_result
+
+
+def test_check_disk_usage_error(): # pylint: disable=too-many-arguments
+
+	def get_info(path):
+		raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path)
+
+	with mock.patch('opsiconfd.application.monitoring.check_opsi_disk_usage.getDiskSpaceUsage', get_info):
+		result = check_opsi_disk_usage(opsiresource="/file/not/found")
+
+	assert json.loads(result.body) == {
+		'message': ('UNKNOWN: ["Not able to check DiskUsage. Error: \'[Errno 2] '
+			'No such file or directory: \'/workspace/opsiconfd_data/static\'\'"]'),
+		'state': 3
+		}
+
+test_data = [
+	(None),
+	({}),
+	([])
+
+]
+
+@pytest.mark.parametrize("return_value", test_data)
+def test_check_disk_usage_no_result(return_value): # pylint: disable=too-many-arguments
+
+	def get_info(path):
+		return return_value
+
+	with mock.patch('opsiconfd.application.monitoring.check_opsi_disk_usage.getDiskSpaceUsage', get_info):
+		result = check_opsi_disk_usage(opsiresource="/file/not/found")
+
+	assert json.loads(result.body) == {
+		'message': ('UNKNOWN: No results get. Nothing to check.'),
+		'state': 3
+		}

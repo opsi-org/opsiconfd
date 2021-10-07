b8e6973b7819f4efb74c8e7e7d4af536148f9031
Jan Schneider <j.schneider@uib.de>
2021-10-07 21:02:46 +0200

    [fix] Fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,13 @@
 import asyncio
 import pytest
 
+from opsiconfd.backend import BackendManager
+
+BackendManager.default_config = {
+	"backendConfigDir": "tests/opsi-config/backends",
+	"dispatchConfigFile": "tests/opsi-config/backendManager/dispatch.conf"
+}
+
 @pytest.fixture(scope='session')
 def event_loop():
 	"""Create an instance of the default event loop for each test case."""

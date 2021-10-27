ac0793b50c1d391c874862fe12417ff12e372c61
Fabian Kalweit <f.kalweit@uib.de>
2021-10-27 11:41:20 +0000

    [chg] webgui addon add const

---
+++ addons/webgui/const.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# opsiconfd is part of the desktop management solution opsi http://www.opsi.org
+# Copyright (c) 2020-2021 uib GmbH <info@uib.de>
+# All rights reserved.
+# License: AGPL-3.0
+"""
+addon webgui - const
+"""
+
+ADDON_ID = "webgui"
+ADDON_NAME = "Webgui"
+ADDON_VERSION = "0.1"

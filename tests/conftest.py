6e3c4a68ccb0b20fb9d1f4abace4b014956c09ce
Jan Schneider <j.schneider@uib.de>
2022-01-27 01:07:34 +0100

    [chg] Remove filterwarnings

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,9 +22,6 @@ from opsiconfd.backend import BackendManager
 from opsiconfd.application.main import app, application_setup
 
 
-warnings.filterwarnings("ignore", category=DeprecationWarning, module=r"aioredis/connection.py", message="There is no current event loop")
-
-
 def emit(*args, **kwargs) -> None:  # pylint: disable=unused-argument
 	pass
 

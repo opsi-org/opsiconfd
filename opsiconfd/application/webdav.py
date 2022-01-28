5e64274900f42a5a445ba0b365388c1042ea0769
Jan Schneider <j.schneider@uib.de>
2022-01-28 12:16:16 +0100

    [new,pub] Add central webdav share /webdav

--- opsiconfd/application/webdav.py
+++ opsiconfd/application/webdav.py
@@ -9,11 +9,15 @@ webdav
 """
 
 import os
+from typing import Dict, List, Optional
 
 from wsgidav import util
 import wsgidav.fs_dav_provider
-from wsgidav.fs_dav_provider import FilesystemProvider
+from wsgidav.dav_error import HTTP_FORBIDDEN, DAVError
+from wsgidav.dav_provider import _DAVResource, DAVCollection, DAVProvider
+from wsgidav.fs_dav_provider import FilesystemProvider, FolderResource
 from wsgidav.wsgidav_app import WsgiDAVApp
+
 
 from .. import __version__
 from ..logging import logger
@@ -23,6 +27,44 @@ from ..wsgi import WSGIMiddleware
 
 
 PUBLIC_FOLDER = "/var/lib/opsi/public"
+BLOCK_SIZE = 64 * 1024
+APP_CONFIG_TEMPLATE = {
+	"simple_dc": {
+		"user_mapping": {"*": True}  # anonymous access
+	},
+	"hotfixes": {
+		"re_encode_path_info": False,  # Encoding is done in opsiconfd.wsgi
+	},
+	"http_authenticator": {
+		# None: dc.simple_dc.SimpleDomainController(user_mapping)
+		"domain_controller": None,
+		"accept_basic": False,  # Allow basic authentication, True or False
+		"accept_digest": False,  # Allow digest authentication, True or False
+		"trusted_auth_header": None,
+	},
+	"verbose": 1,
+	"logging": {
+		"enable_loggers": []
+	},
+	"property_manager": True,  # True: use property_manager.PropertyManager
+	"lock_storage": True,  # True: use lock_manager.LockManager
+	"block_size": BLOCK_SIZE,  # default = 8192
+	"ssl_certificate": True,  # Prevent warning in log
+	"dir_browser": {
+		"show_user": False,
+		"icon": False,
+		"response_trailer": f"opsiconfd {__version__} (uvicorn/WsgiDAV)"
+	},
+	"cors": {
+		"allow_origin": "*"
+	},
+	"provider_mapping": {},
+	"mount_path": None
+}
+
+# Set file buffer size for reading and writing.
+# Sent message chunks will have the same body size.
+wsgidav.fs_dav_provider.BUFFER_SIZE = BLOCK_SIZE
 
 
 # Prevent warning in log
@@ -75,43 +117,34 @@ class IgnoreCaseFilesystemProvider(FilesystemProvider):
 		return file_path
 
 
-def webdav_setup(app):  # pylint: disable=too-many-statements, too-many-branches
-	block_size = 64 * 1024
-	app_config_template = {
-		"simple_dc": {
-			"user_mapping": {"*": True}  # anonymous access
-		},
-		"hotfixes": {
-			"re_encode_path_info": False,  # Encoding is done in opsiconfd.wsgi
-		},
-		"http_authenticator": {
-			# None: dc.simple_dc.SimpleDomainController(user_mapping)
-			"domain_controller": None,
-			"accept_basic": False,  # Allow basic authentication, True or False
-			"accept_digest": False,  # Allow digest authentication, True or False
-			"trusted_auth_header": None,
-		},
-		"verbose": 1,
-		"logging": {
-			"enable_loggers": []
-		},
-		"property_manager": True,  # True: use property_manager.PropertyManager
-		"lock_storage": True,  # True: use lock_manager.LockManager
-		"block_size": block_size,  # default = 8192
-		"ssl_certificate": True,  # Prevent warning in log
-		"dir_browser": {
-			"show_user": False,
-			"icon": False,
-			"response_trailer": f"opsiconfd {__version__} (uvicorn/WsgiDAV)"
-		},
-		"cors": {
-			"allow_origin": "*"
-		}
-	}
-	# Set file buffer size for reading and writing.
-	# Sent message chunks will have the same body size.
-	wsgidav.fs_dav_provider.BUFFER_SIZE = block_size
+class VirtualRootFilesystemCollection(DAVCollection):
+	def __init__(self, environ: dict, provider):
+		DAVCollection.__init__(self, "/", environ)
+		self.provider = provider
 
+	def get_member_names(self) -> List[str]:
+		return [name.lstrip("/") for name in self.provider.provider_mapping if name != "/"]
+
+	def get_member(self, name: str) -> Optional[FolderResource]:
+		if not (provider := self.provider.provider_mapping.get(f"/{name}")):
+			raise DAVError(HTTP_FORBIDDEN)
+		resource = FolderResource(f"/{name}", self.environ, provider.root_folder_path)
+		resource.name = name
+		return resource
+
+
+class VirtualRootFilesystemProvider(DAVProvider):
+	def __init__(self, provider_mapping: Dict[str, FilesystemProvider]) -> None:
+		super().__init__()
+		self.provider_mapping = provider_mapping
+		self.readonly = True
+
+	def get_resource_inst(self, path: str, environ: dict) -> _DAVResource:
+		root = VirtualRootFilesystemCollection(environ, self)
+		return root.resolve("", path)
+
+
+def webdav_setup(app):  # pylint: disable=too-many-statements, too-many-branches, too-many-locals
 	hosts = get_backend().host_getObjects(type='OpsiDepotserver', id=FQDN)  # pylint: disable=no-member
 	if not hosts:
 		logger.warning("Running on host %s which is not a depot server, webdav disabled.", FQDN)
@@ -119,8 +152,8 @@ def webdav_setup(app):  # pylint: disable=too-many-statements, too-many-branches
 
 	depot = hosts[0]
 	depot_id = depot.getId()
-	# self.app_config['depotId'] = depot.getId()
 
+	filesystems = {}
 	try:
 		logger.notice(f"Running on depot server '{depot_id}', exporting repository directory")
 		if not depot.getRepositoryLocalUrl():
@@ -134,11 +167,7 @@ def webdav_setup(app):  # pylint: disable=too-many-statements, too-many-branches
 		if not os.access(path, os.R_OK | os.W_OK | os.X_OK):
 			raise Exception(f"Cannot add webdav content 'repository': permissions on directory '{path}' not sufficient.")
 
-		app_config = dict(app_config_template)
-		app_config["provider_mapping"] = {"/": FilesystemProvider(path)}
-		app_config["mount_path"] = "/repository"
-		repository_dav = WsgiDAVApp(app_config)
-		app.mount("/repository", WSGIMiddleware(repository_dav))
+		filesystems["repository"] = {"path": path, "ignore_case": False}
 	except Exception as exc:  # pylint: disable=broad-except
 		logger.error(exc, exc_info=True)
 
@@ -155,11 +184,7 @@ def webdav_setup(app):  # pylint: disable=too-many-statements, too-many-branches
 		if not os.access(path, os.R_OK | os.W_OK | os.X_OK):
 			raise Exception(f"Cannot add webdav content 'depot': permissions on directory '{path}' not sufficient.")
 
-		app_config = dict(app_config_template)
-		app_config["provider_mapping"] = {"/": IgnoreCaseFilesystemProvider(path, readonly=False)}
-		app_config["mount_path"] = "/depot"
-		depot_dav = WsgiDAVApp(app_config)
-		app.mount("/depot", WSGIMiddleware(depot_dav))
+		filesystems["depot"] = {"path": path, "ignore_case": True}
 	except Exception as exc:  # pylint: disable=broad-except
 		logger.error(exc, exc_info=True)
 
@@ -176,11 +201,7 @@ def webdav_setup(app):  # pylint: disable=too-many-statements, too-many-branches
 		if not os.access(path, os.R_OK | os.W_OK | os.X_OK):
 			raise Exception(f"Cannot add webdav content 'workbench': permissions on directory '{path}' not sufficient.")
 
-		app_config = dict(app_config_template)
-		app_config["provider_mapping"] = {"/": FilesystemProvider(path, readonly=False)}
-		app_config["mount_path"] = "/workbench"
-		workbench_dav = WsgiDAVApp(app_config)
-		app.mount("/workbench", WSGIMiddleware(workbench_dav))
+		filesystems["workbench"] = {"path": path, "ignore_case": False}
 	except Exception as exc:  # pylint: disable=broad-except
 		logger.error(exc, exc_info=True)
 
@@ -192,25 +213,33 @@ def webdav_setup(app):  # pylint: disable=too-many-statements, too-many-branches
 		if not os.access(PUBLIC_FOLDER, os.R_OK | os.W_OK | os.X_OK):
 			raise Exception(f"Cannot add webdav content 'public': permissions on directory '{PUBLIC_FOLDER}' not sufficient.")
 
-		app_config = dict(app_config_template)
-		app_config["provider_mapping"] = {"/": FilesystemProvider(PUBLIC_FOLDER, readonly=True)}
-		app_config["mount_path"] = "/public"
-		public_dav = WsgiDAVApp(app_config)
-		app.mount("/public", WSGIMiddleware(public_dav))
+		filesystems["public"] = {"path": PUBLIC_FOLDER, "ignore_case": False}
 	except Exception as exc:  # pylint: disable=broad-except
 		logger.error(exc, exc_info=True)
 
 	if os.path.isdir("/tftpboot"):
-		path = "/tftpboot"
-		logger.notice(f"Running on depot server '{depot_id}', exporting boot directory")
-		if not os.access(path, os.R_OK | os.X_OK):
-			raise Exception(f"Cannot add webdav content 'boot': permissions on directory '{path}' not sufficient.")
-
 		try:
-			app_config = dict(app_config_template)
-			app_config["provider_mapping"] = {"/": FilesystemProvider(path, readonly=True)}
-			app_config["mount_path"] = "/boot"
-			boot = WsgiDAVApp(app_config)
-			app.mount("/boot", WSGIMiddleware(boot))
+			path = "/tftpboot"
+			logger.notice(f"Running on depot server '{depot_id}', exporting boot directory")
+			if not os.access(path, os.R_OK | os.X_OK):
+				raise Exception(f"Cannot add webdav content 'boot': permissions on directory '{path}' not sufficient.")
+
+			filesystems["boot"] = {"path": path, "ignore_case": True}
 		except Exception as err:  # pylint: disable=broad-except
 			logger.error(err, exc_info=True)
+
+	for name, conf in filesystems.items():
+		app_config = APP_CONFIG_TEMPLATE.copy()
+		prov_class = IgnoreCaseFilesystemProvider if conf["ignore_case"] else FilesystemProvider
+		app_config["provider_mapping"]["/"] = prov_class(conf["path"], readonly=True)
+		app_config["mount_path"] = f"/{name}"
+		app.mount(f"/{name}", WSGIMiddleware(WsgiDAVApp(app_config)))
+
+	app_config = APP_CONFIG_TEMPLATE.copy()
+	for name, conf in filesystems.items():
+		prov_class = IgnoreCaseFilesystemProvider if conf["ignore_case"] else FilesystemProvider
+		app_config["provider_mapping"][f"/{name}"] = prov_class(conf["path"], readonly=False)
+	virt_root_provider = VirtualRootFilesystemProvider(app_config["provider_mapping"])
+	app_config["provider_mapping"]["/"] = virt_root_provider
+	app_config["mount_path"] = "/webdav"
+	app.mount("/webdav", WSGIMiddleware(WsgiDAVApp(app_config)))

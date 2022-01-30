e6d827bf47e93106b76e7e385f74e452c0e89917
Jan Schneider <j.schneider@uib.de>
2022-01-30 11:51:22 +0100

    [chg] More tests

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -8,6 +8,7 @@
 webdav tests
 """
 
+from ipaddress import ip_address
 from OpenSSL.crypto import FILETYPE_PEM, load_privatekey, load_certificate
 
 from opsiconfd import set_contextvars_from_contex
@@ -88,26 +89,63 @@ def test_opsiconfd_backend_get_domain(test_client):  # pylint: disable=redefined
 			test_client.auth = (ADMIN_USER, ADMIN_PASS)
 
 
-def test_opsiconfd_backend_host_get_tls_certificate(test_client):  # pylint: disable=redefined-outer-name
+def test_opsiconfd_backend_host_get_tls_certificate_depot(test_client):  # pylint: disable=redefined-outer-name
 	test_client.auth = (ADMIN_USER, ADMIN_PASS)
-	depot_id = "test-depot-cert.opsi.org"
+	host_id = "test-depot-cert.opsi.org"
 	host_key = "aa768a25913c507dec18560d5924e458"
-	with depot_jsonrpc(test_client, "", depot_id, host_key):
+	with depot_jsonrpc(test_client, "", host_id, host_key):
 		test_client.reset_cookies()
-		test_client.auth = (depot_id, host_key)
+		test_client.auth = (host_id, host_key)
 		try:
 			rpc = {"id": 1, "method": "accessControl_authenticated", "params": []}
 			res = test_client.post("/rpc", json=rpc)
 			res.raise_for_status()
 
-			rpc = {"id": 1, "method": "host_getTLSCertificate", "params": [depot_id]}
+			rpc = {"id": 1, "method": "host_getTLSCertificate", "params": [host_id]}
 			res = test_client.post("/rpc", json=rpc)
 			res.raise_for_status()
 			res = res.json()
 
 			load_privatekey(FILETYPE_PEM, res["result"])
 			cert = load_certificate(FILETYPE_PEM, res["result"])
-			assert cert.get_subject().CN == depot_id
+			assert cert.get_subject().CN == host_id
+		finally:
+			test_client.reset_cookies()
+			test_client.auth = (ADMIN_USER, ADMIN_PASS)
+
+
+def test_opsiconfd_backend_host_get_tls_certificate_client(test_client):  # pylint: disable=redefined-outer-name
+	test_client.auth = (ADMIN_USER, ADMIN_PASS)
+	host_id = "test-client-cert.opsi.org"
+	host_key = "07dec1856aa768a25913c50d5924e458"
+	ip_address = "192.168.1.2"
+	with client_jsonrpc(test_client, "", host_id, host_key, ip_address=ip_address):
+		test_client.reset_cookies()
+		test_client.auth = (host_id, host_key)
+		try:
+			rpc = {"id": 1, "method": "accessControl_authenticated", "params": []}
+			res = test_client.post("/rpc", json=rpc)
+			res.raise_for_status()
+
+			rpc = {"id": 1, "method": "host_getTLSCertificate", "params": [host_id]}
+			res = test_client.post("/rpc", json=rpc)
+			res.raise_for_status()
+			res = res.json()
+
+			load_privatekey(FILETYPE_PEM, res["result"])
+			cert = load_certificate(FILETYPE_PEM, res["result"])
+			sub_alt_name = ""
+			for idx in range(cert.get_extension_count()):
+				ext = cert.get_extension(idx)
+				if ext.get_short_name() == b"subjectAltName":
+					sub_alt_name = str(ext)
+			assert f"IP Address:{ip_address}" in sub_alt_name
+
+			rpc = {"id": 1, "method": "host_getTLSCertificate", "params": ["test-client2-cert.opsi.org"]}
+			res = test_client.post("/rpc", json=rpc)
+			res.raise_for_status()
+			res = res.json()
+			assert res["error"]["class"] == "BackendPermissionDeniedError"
 		finally:
 			test_client.reset_cookies()
 			test_client.auth = (ADMIN_USER, ADMIN_PASS)

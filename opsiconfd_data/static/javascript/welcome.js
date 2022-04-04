48264908e3f6ef97b828ffb081188d16993fb3d9
Fabian Kalweit <f.kalweit@uib.de>
2022-04-04 12:56:21 +0000

    [chg] spaces -> tabs

--- opsiconfd_data/static/javascript/welcome.js
+++ opsiconfd_data/static/javascript/welcome.js
@@ -1,37 +1,37 @@
 // opsi welcome.js
 
 function onLoad() {
-    let userLang = navigator.language || navigator.userLanguage;
-    let lang = "en"
-    if (userLang == "de-DE") {
-        lang = "de"
-    }
+	let userLang = navigator.language || navigator.userLanguage;
+	let lang = "en"
+	if (userLang == "de-DE") {
+		lang = "de"
+	}
 
-    content = document.getElementsByClassName(lang)
-    console.log(content);
-    console.log(typeof (content));
-    for (var i = 0; i < content.length; i++) {
-        content[i].style.visibility = "visible";
-        content[i].style.display = "block";
-    }
+	content = document.getElementsByClassName(lang)
+	console.log(content);
+	console.log(typeof (content));
+	for (var i = 0; i < content.length; i++) {
+		content[i].style.visibility = "visible";
+		content[i].style.display = "block";
+	}
 }
 
 
 function deactivateWelcomePage() {
-    let request = new XMLHttpRequest();
-    request.open("POST", "/welcome/deactivate");
-    request.addEventListener('load', function (event) {
-        if (request.status >= 200 && request.status < 300) {
-            result = request.responseText;
-            result = JSON.parse(result);
-            console.log(result);
-            let skip = document.getElementById("skip");
-            skip.disabled = true;
-            return result;
-        } else {
-            console.warn(request.statusText, request.responseText);
-            return request.statusText;
-        }
-    });
-    request.send()
+	let request = new XMLHttpRequest();
+	request.open("POST", "/welcome/deactivate");
+	request.addEventListener('load', function (event) {
+		if (request.status >= 200 && request.status < 300) {
+			result = request.responseText;
+			result = JSON.parse(result);
+			console.log(result);
+			let skip = document.getElementById("skip");
+			skip.disabled = true;
+			return result;
+		} else {
+			console.warn(request.statusText, request.responseText);
+			return request.statusText;
+		}
+	});
+	request.send()
 }

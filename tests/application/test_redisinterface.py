ed865251919f0808926113f30fd510c652d94d6d
Fabian Kalweit <f.kalweit@uib.de>
2022-06-27 12:24:00 +0000

    [fix] typo

--- tests/application/test_redisinterface.py
+++ tests/application/test_redisinterface.py
@@ -1,12 +1,12 @@
-# # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-# # opsiconfd is part of the desktop management solution opsi http://www.opsi.org
-# # Copyright (c) 2020-2021 uib GmbH <info@uib.de>
-# # All rights reserved.
-# # License: AGPL-3.0
-# """
-# test application redis interface
-# """
+# opsiconfd is part of the desktop management solution opsi http://www.opsi.org
+# Copyright (c) 2020-2021 uib GmbH <info@uib.de>
+# All rights reserved.
+# License: AGPL-3.0
+"""
+test application redis interface
+"""
 
 import time
 from socket import getfqdn

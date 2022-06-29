3b1108ff6c075f050dd046bc38379bb43351d430
Fabian Kalweit <f.kalweit@uib.de>
2022-06-29 08:32:20 +0000

    [fix] create link header call in rest_api

--- opsiconfd/rest.py
+++ opsiconfd/rest.py
@@ -236,7 +236,7 @@ def rest_api(default_error_status_code: Union[Callable, int, None] = None):  # p
 			try:  # pylint: disable=too-many-branches,too-many-nested-blocks
 				result = await exec_func(func, *args, **kwargs)
 				if isinstance(result, RESTResponse):  # pylint: disable=no-else-return
-					headers = create_link_header(result.total, kwargs.get("commons"), kwargs.get("request"))
+					headers = create_link_header(result.total, kwargs.get("commons"), kwargs.get("request").url)
 					result.headers.update(headers)
 					return result.to_jsonresponse()
 				# Deprecated dict response.
@@ -244,7 +244,7 @@ def rest_api(default_error_status_code: Union[Callable, int, None] = None):  # p
 					warnings.warn("opsi REST api data dict ist deprecated. All opsi api functions should return a RESTResponse.", DeprecationWarning)
 					if result.get("data"):
 						content = result.get("data")
-					headers = create_link_header(result.get("total"), kwargs.get("commons"), kwargs.get("request"))
+					headers = create_link_header(result.get("total"), kwargs.get("commons"), kwargs.get("request").url)
 				else:
 					content = result
 				return JSONResponse(content=content, status_code=http_status, headers=headers)

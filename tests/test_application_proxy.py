f3c28a3f8b2afb7905de763d1a4843084d885e2e
Jan Schneider <j.schneider@uib.de>
2022-07-04 12:31:47 +0000

    [fix] Fix proxy tests

--- tests/test_application_proxy.py
+++ tests/test_application_proxy.py
@@ -33,9 +33,9 @@ def test_reverse_proxy_request(tmp_path, test_client):  # pylint: disable=redefi
 		mock.patch("opsiconfd.application.proxy.proxy_logger.isEnabledFor", lambda lvl: True),
 		http_test_server(log_file=log_file) as server,
 	):
-		ReverseProxy(app, "/test_reverse_proxy_request", f"http://localhost:{server.port}")
+		ReverseProxy(app, "/test_reverse_proxy_request1", f"http://localhost:{server.port}")
 
-		res = test_client.get("/test_reverse_proxy_request/test/get", auth=(ADMIN_USER, ADMIN_PASS))
+		res = test_client.get("/test_reverse_proxy_request1/test/get", auth=(ADMIN_USER, ADMIN_PASS))
 		assert res.status_code == 200
 
 		log = log_file.read_text(encoding="utf-8")
@@ -65,10 +65,10 @@ def test_forward_cookie(tmp_path, test_client):  # pylint: disable=redefined-out
 	log_file = tmp_path / "request.log"
 
 	with http_test_server(log_file=log_file) as server:
-		ReverseProxy(app, "/test_forward_cookie", f"http://localhost:{server.port}", forward_cookies=None)
+		ReverseProxy(app, "/test_forward_cookie1", f"http://localhost:{server.port}", forward_cookies=None)
 
 		headers = {"Cookie": "test-cookie=123"}
-		res = test_client.get("/test_forward_cookie/test/get", auth=(ADMIN_USER, ADMIN_PASS), headers=headers)
+		res = test_client.get("/test_forward_cookie1/test/get", auth=(ADMIN_USER, ADMIN_PASS), headers=headers)
 		assert res.status_code == 200
 
 		log = log_file.read_text(encoding="utf-8")

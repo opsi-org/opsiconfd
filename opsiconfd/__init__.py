e5e1516775ee6daa9ff22eb39d18eec01f4f602f
Jan Schneider <j.schneider@uib.de>
2022-07-12 17:00:36 +0000

    [chg,pub] Update python packages
      • Updating urllib3 (1.26.9 -> 1.26.10)
      • Updating cryptography (37.0.3 -> 37.0.4)
      • Updating astroid (2.11.6 -> 2.11.7)
      • Updating hypothesis (6.49.1 -> 6.50.1)
      • Updating jsonschema (4.6.1 -> 4.7.2)
      • Updating rich (12.4.4 -> 12.5.1)
      • Updating tomlkit (0.11.0 -> 0.11.1)
      • Updating coverage (6.4.1 -> 6.4.2)
      • Updating pyinstaller-hooks-contrib (2022.7 -> 2022.8)
      • Updating python-opsi-common (4.2.0.287 -> 4.2.0.288)
      • Updating types-urllib3 (1.26.15 -> 1.26.16)
      • Updating opsi-dev-tools (1.1.11 -> 1.1.12)
      • Updating orjson (3.7.6 -> 3.7.7)
      • Updating python-opsi (4.2.0.299 -> 4.2.0.300)
      • Updating types-redis (4.3.3 -> 4.3.4)

--- opsiconfd/__init__.py
+++ opsiconfd/__init__.py
@@ -8,7 +8,7 @@
 The opsi configuration service.
 """
 
-__version__ = "4.2.0.275"
+__version__ = "4.2.0.276"
 
 from contextvars import Context, ContextVar
 from typing import TYPE_CHECKING, Dict, Optional

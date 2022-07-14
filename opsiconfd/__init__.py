d64b87d74a76ede1fb4ca324916cc7a1d8d470ec
Jan Schneider <j.schneider@uib.de>
2022-07-14 09:31:25 +0000

    [chg,pub] Update python packages
      • Updating docutils (0.17.1 -> 0.18.1)
      • Installing mailbits (0.2.1)
      • Updating sphinx (4.5.0 -> 5.0.2)
      • Updating pefile (2021.9.3 -> 2022.5.30)
      • Updating pypi-simple (0.9.0 -> 0.10.0)
      • Updating opsi-dev-tools (1.1.12 -> 1.1.13)
      • Updating pyinstaller (5.1 -> 5.2)
      • Updating python-opsi (4.2.0.301 -> 4.2.0.302)

--- opsiconfd/__init__.py
+++ opsiconfd/__init__.py
@@ -8,7 +8,7 @@
 The opsi configuration service.
 """
 
-__version__ = "4.2.0.277"
+__version__ = "4.2.0.278"
 
 from contextvars import Context, ContextVar
 from typing import TYPE_CHECKING, Dict, Optional

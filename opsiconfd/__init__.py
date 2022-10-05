3e9f47620d311c52edee264c800498404734d840
Jan Schneider <j.schneider@uib.de>
2022-10-05 10:04:17 +0000

    [fix] Fix skip-setup

--- opsiconfd/__init__.py
+++ opsiconfd/__init__.py
@@ -8,7 +8,7 @@
 The opsi configuration service.
 """
 
-__version__ = "4.2.0.288"
+__version__ = "4.2.0.290"
 
 from contextvars import Context, ContextVar
 from typing import TYPE_CHECKING, Dict, Optional

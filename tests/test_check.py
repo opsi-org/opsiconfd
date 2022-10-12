2c77f4016758dcf52a3837221959b4eef86ddf5c
Fabian Kalweit <f.kalweit@uib.de>
2022-10-12 11:58:27 +0000

    [chg] check tests add captured_function_output

--- tests/test_check.py
+++ tests/test_check.py
@@ -11,23 +11,30 @@ check tests
 
 import io
 import sys
+from typing import Any, Callable, Dict
 from unittest import mock
 
 from colorama import Fore, Style  # type: ignore[import]
 from redis.exceptions import ConnectionError as RedisConnectionError
 
-from opsiconfd.check import check_redis
+from opsiconfd.check import check_mysql, check_redis
+
+
+def captured_function_output(func: Callable, args: Dict[str, Any]) -> Dict[str, Any]:
+	captured_output = io.StringIO()
+	sys.stdout = captured_output
+	result = func(**args)
+	sys.stdout = sys.__stdout__
+
+	return {"captured_output": captured_output.getvalue(), "data": result}
 
 
 def test_check_redis() -> None:
 
-	captured_output = io.StringIO()
-	sys.stdout = captured_output
-	result = check_redis(print_messages=True)
-	sys.stdout = sys.__stdout__
+	result = captured_function_output(check_redis, {"print_messages": True})
 
 	assert (
-		captured_output.getvalue()
+		result.get("captured_output")
 		== Fore.WHITE
 		+ Style.BRIGHT
 		+ "Checking redis..."
@@ -39,29 +46,24 @@ def test_check_redis() -> None:
 		+ Style.RESET_ALL
 		+ "\n"
 	)
-	assert result.get("status") is not None
-	assert result["status"] == "ok"
+	data = result.get("data", {})
+	assert data.get("status") is not None
+	assert data["status"] == "ok"
 
-	captured_output = io.StringIO()
-	sys.stdout = captured_output
-	result = check_redis(print_messages=False)
-	sys.stdout = sys.__stdout__
+	result = captured_function_output(check_redis, {"print_messages": False})
 
-	assert captured_output.getvalue() == ""
-	assert result.get("status") is not None
-	assert result["status"] == "ok"
+	assert result.get("captured_output") == ""
+	data = result.get("data", {})
+	assert data.get("status") is not None
+	assert data["status"] == "ok"
 
 
 def test_check_redis_error() -> None:
 
 	with mock.patch("opsiconfd.utils.get_redis_connection", side_effect=RedisConnectionError("Redis test error")):
-		captured_output = io.StringIO()
-		sys.stdout = captured_output
-		result = check_redis(print_messages=True)
-		sys.stdout = sys.__stdout__
-
+		result = captured_function_output(check_redis, {"print_messages": True})
 		assert (
-			captured_output.getvalue()
+			result.get("captured_output")
 			== Fore.WHITE
 			+ Style.BRIGHT
 			+ "Checking redis..."
@@ -73,6 +75,54 @@ def test_check_redis_error() -> None:
 			+ Style.RESET_ALL
 			+ "\n"
 		)
-		assert result.get("status") is not None
-		assert result["status"] == "error"
-		assert result["details"] == "Redis test error"
+		data = result.get("data", {})
+		assert data.get("status") is not None
+		assert data["status"] == "error"
+		assert data["details"] == "Redis test error"
+
+
+def test_check_mysql() -> None:
+
+	result = captured_function_output(check_mysql, {"print_messages": True})
+
+	assert (
+		result.get("captured_output")
+		== Fore.WHITE
+		+ Style.BRIGHT
+		+ "Checking mysql..."
+		+ Style.RESET_ALL
+		+ "\n"
+		+ Fore.GREEN
+		+ Style.BRIGHT
+		+ "Connection to mysql is working."
+		+ Style.RESET_ALL
+		+ "\n"
+	)
+	data = result.get("data", {})
+	assert data.get("status") is not None
+	assert data["status"] == "ok"
+	assert data["details"] == "Connection to mysql is working."
+
+
+def test_check_mysql_error() -> None:
+
+	with mock.patch("opsiconfd.check.execute", side_effect=RuntimeError("Command '[...]' failed (1):\nMysql test error")):
+		result = captured_function_output(check_mysql, {"print_messages": True})
+
+		assert (
+			result.get("captured_output")
+			== Fore.WHITE
+			+ Style.BRIGHT
+			+ "Checking mysql..."
+			+ Style.RESET_ALL
+			+ "\n"
+			+ Fore.RED
+			+ Style.BRIGHT
+			+ "Could not connect to mysql: Mysql test error"
+			+ Style.RESET_ALL
+			+ "\n"
+		)
+		data = result.get("data", {})
+		assert data.get("status") is not None
+		assert data["status"] == "error"
+		assert data["details"] == "Mysql test error"

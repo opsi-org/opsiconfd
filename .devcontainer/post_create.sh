9a5b4785e6f66222ffb34835f7a144f70c341ebe
Jan Schneider <j.schneider@uib.de>
2022-11-28 16:50:39 +0000

    [chg] Set vim as editor

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,15 +1,6 @@
 echo "* Running as $(whoami)"
 
-install_jemalloc() {
-	echo "* Installing jemalloc"
-	cd /tmp
-	wget https://github.com/jemalloc/jemalloc/releases/download/$JEMALLOC_VERSION/jemalloc-$JEMALLOC_VERSION.tar.bz2
-	tar xvjf jemalloc-$JEMALLOC_VERSION.tar.bz2
-	cd jemalloc-$JEMALLOC_VERSION
-	./configure
-	make
-	sudo make install
-}
+sudo update-alternatives --set editor /usr/bin/vim.basic
 
 sudo mkdir -p /var/log/opsi
 sudo mkdir -p /var/lib/opsi/depot
@@ -26,5 +17,3 @@ sudo chown -R $DEV_USER /workspace
 /workspace/scripts/setup-grafana.sh
 /workspace/scripts/setup-mysql.sh
 /workspace/scripts/setup-redis.sh
-
-# [ -n "$JEMALLOC_VERSION" ] && install_jemalloc

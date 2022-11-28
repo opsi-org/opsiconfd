b3f44da1f0ffad8beb78cff6c6e2ddda489a25a1
Jan Schneider <j.schneider@uib.de>
2022-11-28 23:47:30 +0000

    [chg] Fix tests

--- tests/test_backend_rpc_extender.py
+++ tests/test_backend_rpc_extender.py
@@ -8,8 +8,8 @@
 test opsiconfd.backend.rpc.extender
 """
 
-
 from datetime import datetime
+from pathlib import Path
 
 from opsiconfd.backend.rpc.extender import RPCExtenderMixin
 
@@ -20,14 +20,20 @@ class Backend(RPCExtenderMixin):  # pylint: disable=too-few-public-methods
 	pass
 
 
-def test_extender_loading() -> None:
-	with get_config({"extension_config_dir": "tests/data/opsi-config/backendManager/extend.d"}):
+def test_extender_loading(tmp_path: Path) -> None:
+	ext1 = tmp_path / "1.conf"
+	ext1.write_text("def extend1(self):\n\treturn 1\n\ndef extend2(self, param2):\n\treturn None\n", encoding="utf-8")
+	ext2 = tmp_path / "2.conf"
+	ext2.write_text("def extend2(self, param2):\n\treturn 2\n\ndef extend3(self, param3):\n\treturn 3\n", encoding="utf-8")
+
+	with get_config({"extension_config_dir": str(tmp_path)}):
 		extender = Backend()
 		count_rpc_methods = 0
 		for val in extender.__dict__.values():
 			if hasattr(val, "rpc_method"):
 				count_rpc_methods += 1
-		assert count_rpc_methods == 134
+		assert count_rpc_methods == 3
 
-		val = extender.getServiceTime()  # type: ignore[attr-defined]  # pylint: disable=no-member
-		assert str(datetime.now())[:16] == val[:16]
+		assert extender.extend1() == 1  # type: ignore[attr-defined]  # pylint: disable=no-member
+		assert extender.extend2("a") == 2  # type: ignore[attr-defined]  # pylint: disable=no-member
+		assert extender.extend3("b") == 3  # type: ignore[attr-defined]  # pylint: disable=no-member

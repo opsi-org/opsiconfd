987be40cbe41526ff5152951edbf3861496fa6d5
Jan Schneider <j.schneider@uib.de>
2022-12-29 22:30:44 +0000

    [chg] Add session tests

---
+++ tests/test_session.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# opsiconfd is part of the desktop management solution opsi http://www.opsi.org
+# Copyright (c) 2020-2021 uib GmbH <info@uib.de>
+# All rights reserved.
+# License: AGPL-3.0
+"""
+session tests
+"""
+
+from opsiconfd.session import OPSISession
+
+from .utils import clean_redis  # pylint: disable=unused-import
+
+
+def test_session_serialize() -> None:
+	client_addr = "172.10.11.12"
+	client_port = 23638
+	session = OPSISession(client_addr=client_addr, client_port=client_port)
+	data = session.serialize()
+	session2 = OPSISession.from_serialized(data)
+	assert session.serialize() == session2.serialize()

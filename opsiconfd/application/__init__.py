5bcb6bf873c96989163e7a0e22b31c6a2b68ec7e
Jan Schneider <j.schneider@uib.de>
2023-01-14 11:03:26 +0000

    [chg] More debug

--- opsiconfd/application/__init__.py
+++ opsiconfd/application/__init__.py
@@ -15,7 +15,7 @@ import time
 from contextlib import nullcontext
 from dataclasses import asdict, dataclass, field
 from ipaddress import ip_network
-from threading import Event, Lock
+from threading import Event, current_thread
 from typing import Any, Callable, Type, TypeVar
 
 from fastapi import FastAPI
@@ -150,10 +150,10 @@ class OpsiconfdApp(FastAPI):
 	def set_app_state(self, app_state: AppState, wait_accomplished: float | None = 30.0) -> None:
 		app_state.accomplished = False
 		self.app_state_updated.clear()
-		print("SET APP STATE START", app_state)
+		print(current_thread().ident, "SET APP STATE START", app_state)
 		with redis_lock("app-state", acquire_timeout=2.0, lock_timeout=5.0):
 			self.store_app_state_in_redis(app_state)
-		print("SET APP STATE DONE", app_state)
+		print(current_thread().ident, "SET APP STATE DONE", app_state)
 		if wait_accomplished is not None and wait_accomplished > 0:
 			self.wait_for_app_state(app_state, wait_accomplished)
 
@@ -227,16 +227,16 @@ class OpsiconfdApp(FastAPI):
 					init_app_state = (init_app_state,)
 				if not app_state or app_state.type not in [a.type for a in init_app_state]:
 					app_state = init_app_state[0]
-				await self.async_store_app_state_in_redis(app_state)  # type: ignore[arg-type]
+					await self.async_store_app_state_in_redis(app_state)  # type: ignore[arg-type]
 				if app_state:
 					self._app_state = app_state
 
 		interval = 1
 		while not self._manager_task_should_stop:
-			print("loop")
+			print(current_thread().ident, "loop")
 			cur_state = self._app_state
 			app_state = await self.load_app_state_from_redis(update_accomplished=manager_mode)
-			print("loop app_state", app_state)
+			print(current_thread().ident, "loop app_state", app_state)
 			if app_state:
 				self.app_state_updated.set()
 				self._app_state = app_state

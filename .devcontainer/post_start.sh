f90424058fa94f2b705bf9aee506dbb18845ea33
Jan Schneider <j.schneider@uib.de>
2023-01-24 06:22:11 +0000

    [fix] Fix git clone

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -19,7 +19,7 @@ sudo opsi-set-rights
 
 echo "* Install git hooks"
 cd $HOME
-git clone https://oauth2:UqZXUJsgG4dBGLBbTjDM@gitlab.uib.gmbh/uib/opsi-git-hooks.git .opsi-git-hooks
+git clone .opsi-git-hooks
 cd /workspace
 opsi-dev-tool git-hooks --install
 

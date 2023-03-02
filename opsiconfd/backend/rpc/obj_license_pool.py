7958ce56a4a3f8e8a7ad610a36a7b7e5ee505378
Jan Schneider <j.schneider@uib.de>
2023-03-02 06:29:58 +0000

    [fix] Fix JOIN

--- opsiconfd/backend/rpc/obj_license_pool.py
+++ opsiconfd/backend/rpc/obj_license_pool.py
@@ -107,7 +107,10 @@ class RPCLicensePoolMixin(Protocol):
 			"productIds": f'GROUP_CONCAT(`productId` SEPARATOR "{self._mysql.record_separator}")',
 		}
 		return self._mysql.get_objects(
-			table="`LICENSE_POOL` LEFT JOIN `PRODUCT_ID_TO_LICENSE_POOL` ON `LICENSE_POOL`.`licensePoolId` = `LICENSE_POOL`.`licensePoolId`",
+			table=(
+				"`LICENSE_POOL` LEFT JOIN `PRODUCT_ID_TO_LICENSE_POOL` "
+				"ON `LICENSE_POOL`.`licensePoolId` = `PRODUCT_ID_TO_LICENSE_POOL`.`licensePoolId`"
+			),
 			object_type=LicensePool,
 			aggregates=aggregates,
 			ace=ace,

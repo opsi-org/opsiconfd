77f89a8b43df721e56e8df0e6e994177071867a3
Fabian Kalweit <f.kalweit@uib.de>
2023-04-13 05:21:38 +0000

    [chg] just raise error again

--- opsiconfd/session.py
+++ opsiconfd/session.py
@@ -1059,7 +1059,7 @@ async def authenticate_user_auth_module(scope: Scope) -> None:
 async def authenticate(scope: Scope, username: str, password: str, mfa_otp: str | None = None) -> None:
 	try:
 		await _authenticate(scope, username, password, mfa_otp)
-	except BackendAuthenticationError as auth_error:
+	except BackendAuthenticationError:
 		cmd = (
 			f"ts.add {config.redis_key('stats')}:client:failed_auth:{ip_address_to_redis_key(scope['client'][0])} "
 			f"* 1 RETENTION 86400000 LABELS client_addr {scope['client'][0]}"
@@ -1068,9 +1068,7 @@ async def authenticate(scope: Scope, username: str, password: str, mfa_otp: str 
 		redis = await async_redis_client()
 		await redis.execute_command(cmd)  # type: ignore[no-untyped-call]
 		await asyncio.sleep(0.2)
-		raise BackendAuthenticationError(
-			message=auth_error.message, status_code=auth_error.status_code, content=auth_error.content
-		) from auth_error
+		raise
 
 
 async def _authenticate(  # pylint: disable=unused-argument,too-many-branches,too-many-statements

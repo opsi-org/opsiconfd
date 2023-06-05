effaa3c35934a223e0874cc16a2a56bced24c071
Fabian Kalweit <f.kalweit@uib.de>
2023-06-05 17:00:44 +0200

    [chg] new version

--- opsiconfd/__init__.py
+++ opsiconfd/__init__.py
@@ -8,7 +8,7 @@
 The opsi configuration service.
 """
 
-__version__ = "4.2.0.309"
+__version__ = "4.2.0.310"
 
 from contextvars import Context, ContextVar
 from typing import TYPE_CHECKING, Dict, Optional

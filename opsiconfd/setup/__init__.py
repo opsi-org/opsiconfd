f639bdcfeb684ca5aff7cb0fb04dff72bc8b361f
Nils Doerrer <n.doerrer@uib.de>
2023-06-30 12:13:44 +0000

    [new] workbench_buildPackage now respects the markerfile...

--- opsiconfd/setup/__init__.py
+++ opsiconfd/setup/__init__.py
@@ -17,9 +17,7 @@ from uuid import UUID
 from opsicommon.client.opsiservice import ServiceClient  # type: ignore[import]
 from opsicommon.exceptions import OpsiServiceConnectionError
 from opsicommon.objects import OpsiDepotserver  # type: ignore[import]
-from opsicommon.server.setup import (
-	setup_users_and_groups as po_setup_users_and_groups,  # type: ignore[import]
-)
+from opsicommon.server.setup import setup_users_and_groups as po_setup_users_and_groups  # type: ignore[import]
 from opsicommon.types import forceHostId
 from rich import print as rich_print
 from rich.prompt import Confirm, Prompt

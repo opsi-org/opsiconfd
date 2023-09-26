c8682da8f3fb9cf5cd73c0b0d6862af163a5d314
Fabian Kalweit <f.kalweit@uib.de>
2023-09-26 13:08:01 +0000

    [chg] add more tests

--- tests/backend/test_transfer_slots.py
+++ tests/backend/test_transfer_slots.py
@@ -8,22 +8,29 @@
 webdav tests
 """
 from typing import Generator
-from unittest.mock import Mock, patch
 
 import pytest
 
-from opsiconfd.backend.rpc.general import TRANSFER_SLOT_CONFIG, TRANSFER_SLOT_MAX, TransferSlot
+from opsicommon.exceptions import BackendPermissionDeniedError
+
+from opsiconfd.backend.rpc.depot import TRANSFER_SLOT_CONFIG, TRANSFER_SLOT_MAX, TransferSlot
 from opsiconfd.backend.rpc.main import UnprotectedBackend
+from opsiconfd.config import Config
+from opsiconfd.redis import decode_redis_result
+from tests.backend.rpc.test_config_state import _create_clients_and_depot
 
 from ..utils import (  # pylint: disable=unused-import
 	ADMIN_PASS,
 	ADMIN_USER,
 	OpsiconfdTestClient,
 	backend,
-	client_jsonrpc,
 	sync_clean_redis,
+	sync_redis_client,
 	test_client,
+	config,
 )
+
+TEST_SLOT_ID = "17676023-8426-4094-8ac7-ef4c22ac9803"
 
 
 @pytest.fixture(autouse=True)
@@ -42,118 +49,197 @@ def clean_configs_and_objects(backend: UnprotectedBackend) -> Generator:  # pyli
 
 # Acquiring a transfer slot when there are available slots.
 def test_acquire_transfer_slot_available_slots(test_client: OpsiconfdTestClient) -> None:  # pylint: disable=redefined-outer-name
-	# Mock the session
-	session_mock = Mock()
-	session_mock.authenticated = True
-	session_mock.host = None
-	session_mock.client_addr = "127.0.0.1"
-	patch("opsiconfd.contextvar_client_session.get", return_value=session_mock)
-
-	# Call the method under test
 	test_client.auth = (ADMIN_USER, ADMIN_PASS)
-	client_id = "test-client-dom.opsi.org"
-	host_key = "76768a28560d5924e4587dec5913c501"
-	with client_jsonrpc(test_client, "", client_id, host_key):
-		rpc = {"id": 1, "method": "service_acquireTransferSlot", "params": ["depot1.uib.test"]}
-		res = test_client.post("/rpc", json=rpc)
+	rpc = {"id": 1, "method": "depot_acquireTransferSlot", "params": ["depot1.uib.test", "client1.uib.test"]}
+	res = test_client.post("/rpc", json=rpc)
 	result = res.json()
 
 	# Assert the result
 	assert result["result"].get("slot_id") is not None
 	assert result["result"].get("depot_id") == "depot1.uib.test"
+	assert result["result"].get("client_id") == "client1.uib.test"
 	assert result["result"].get("retry_after") is None
 
 
 def test_acquire_transfer_slot_with_slot_id(test_client: OpsiconfdTestClient) -> None:  # pylint: disable=redefined-outer-name
-	# Mock the session
-	session_mock = Mock()
-	session_mock.authenticated = True
-	session_mock.host = None
-	session_mock.client_addr = "127.0.0.1"
-	patch("opsiconfd.contextvar_client_session.get", return_value=session_mock)
-
-	# Call the method under test
 	test_client.auth = (ADMIN_USER, ADMIN_PASS)
-	client_id = "test-client-dom.opsi.org"
-	host_key = "76768a28560d5924e4587dec5913c501"
 	for i in range(12):  # pylint: disable=unused-variable
-		with client_jsonrpc(test_client, "", client_id, host_key):
-			rpc = {"id": 1, "method": "service_acquireTransferSlot", "params": ["depot1.uib.test", "depot1.uib.test-slot1"]}
-			res = test_client.post("/rpc", json=rpc)
+		rpc = {"id": 1, "method": "depot_acquireTransferSlot", "params": ["depot1.uib.test", "client1.uib.test", TEST_SLOT_ID]}
+		res = test_client.post("/rpc", json=rpc)
 		result = res.json()
-
+		print(result)
 		# Assert the result
-		assert result["result"].get("slot_id") == "depot1.uib.test-slot1"
+		assert result["result"].get("slot_id") == TEST_SLOT_ID
 		assert result["result"].get("depot_id") == "depot1.uib.test"
+		assert result["result"].get("client_id") == "client1.uib.test"
 		assert result["result"].get("retry_after") is None
 
 
 def test_acquire_transfer_slot_max(test_client: OpsiconfdTestClient) -> None:  # pylint: disable=redefined-outer-name
-	# Mock the session
-	session_mock = Mock()
-	session_mock.authenticated = True
-	session_mock.host = None
-	session_mock.client_addr = "127.0.0.1"
-	patch("opsiconfd.contextvar_client_session.get", return_value=session_mock)
-
+	test_client.auth = (ADMIN_USER, ADMIN_PASS)
 	for i in range(TRANSFER_SLOT_MAX):
 		print(i)
 		# Call the method under test
 		test_client.auth = (ADMIN_USER, ADMIN_PASS)
-		client_id = "test-client-dom.opsi.org"
-		host_key = "76768a28560d5924e4587dec5913c501"
-		with client_jsonrpc(test_client, "", client_id, host_key):
-			rpc = {"id": 1, "method": "service_acquireTransferSlot", "params": ["depot1.uib.test", f"depot1.uib.test-slot{i}"]}
-			res = test_client.post("/rpc", json=rpc)
+		rpc = {
+			"id": 1,
+			"method": "depot_acquireTransferSlot",
+			"params": ["depot1.uib.test", "client1.uib.test"],
+		}
+		res = test_client.post("/rpc", json=rpc)
 		result = res.json()
-
+		print(result)
 		# Assert the result
-		assert result["result"].get("slot_id") == f"depot1.uib.test-slot{i}"
+		assert result["result"].get("slot_id") is not None
 		assert result["result"].get("depot_id") == "depot1.uib.test"
+		assert result["result"].get("client_id") == "client1.uib.test"
 		assert result["result"].get("retry_after") is None
 
-	with client_jsonrpc(test_client, "", client_id, host_key):
-		rpc = {"id": 1, "method": "service_acquireTransferSlot", "params": ["depot1.uib.test", "depot1.uib.test-slot10"]}
-		res = test_client.post("/rpc", json=rpc)
+	rpc = {
+		"id": 1,
+		"method": "depot_acquireTransferSlot",
+		"params": ["depot1.uib.test", "client1.uib.test"],
+	}
+	res = test_client.post("/rpc", json=rpc)
 	result = res.json()
-
+	print(result)
 	# Assert the result
 	assert result["result"].get("slot_id") is None
 	assert result["result"].get("depot_id") is None
+	assert result["result"].get("client_id") is None
+	assert result["result"].get("retry_after") is not None
+
+	rpc = {
+		"id": 1,
+		"method": "depot_acquireTransferSlot",
+		"params": ["depot1.uib.test", "client1.uib.test", TEST_SLOT_ID],
+	}
+	res = test_client.post("/rpc", json=rpc)
+	result = res.json()
+	print(result)
+	# Assert the result
+	assert result["result"].get("slot_id") is None
+	assert result["result"].get("depot_id") is None
+	assert result["result"].get("client_id") is None
+	assert result["result"].get("retry_after") is not None
+
+
+def test_acquire_transfer_slot_max_config(  # pylint: disable=redefined-outer-name
+	test_client: OpsiconfdTestClient, backend: UnprotectedBackend
+) -> None:
+	test_client.auth = (ADMIN_USER, ADMIN_PASS)
+
+	clients, depot = _create_clients_and_depot(test_client)
+
+	backend.config_create(id=TRANSFER_SLOT_CONFIG, defaultValues=[5])
+	backend.configState_create(configId=TRANSFER_SLOT_CONFIG, objectId=depot["id"], values=[5])
+
+	for i in range(5):
+		print(i)
+		# Call the method under test
+		test_client.auth = (ADMIN_USER, ADMIN_PASS)
+		rpc = {
+			"id": 1,
+			"method": "depot_acquireTransferSlot",
+			"params": [depot["id"], clients[1]["id"]],
+		}
+		res = test_client.post("/rpc", json=rpc)
+		result = res.json()
+		print(result)
+		# Assert the result
+		assert result["result"].get("slot_id") is not None
+		assert result["result"].get("depot_id") == depot["id"]
+		assert result["result"].get("client_id") == clients[1]["id"]
+		assert result["result"].get("retry_after") is None
+
+	rpc = {
+		"id": 1,
+		"method": "depot_acquireTransferSlot",
+		"params": [depot["id"], clients[1]["id"]],
+	}
+	res = test_client.post("/rpc", json=rpc)
+	result = res.json()
+	print(result)
+	# Assert the result
+	assert result["result"].get("slot_id") is None
+	assert result["result"].get("depot_id") is None
+	assert result["result"].get("client_id") is None
 	assert result["result"].get("retry_after") is not None
 
 
 def test_create_transfer_slot_with_depot_id() -> None:
-	depot_id = "depot1"
-	transfer_slot = TransferSlot(depot_id)
+	depot_id = "depot1.uib.test"
+	client_id = "client1.uib.test"
+	transfer_slot = TransferSlot(depot_id, client_id)
 	assert transfer_slot.depot_id == depot_id
+	assert transfer_slot.client_id == client_id
 	assert transfer_slot.slot_id is not None
 	assert transfer_slot.retry_after is None
 
 
 def test_create_transfer_slot_with_depot_id_and_slot_id() -> None:
-	depot_id = "depot1"
-	slot_id = "slot1"
-	transfer_slot = TransferSlot(depot_id, slot_id)
+	depot_id = "depot1.uib.test"
+	client_id = "client1.uib.test"
+	slot_id = TEST_SLOT_ID
+	transfer_slot = TransferSlot(depot_id, client_id, slot_id)
 	assert transfer_slot.depot_id == depot_id
-	assert transfer_slot.slot_id == slot_id
+	assert transfer_slot.client_id == client_id
+	assert str(transfer_slot.slot_id) == slot_id
 	assert transfer_slot.retry_after is None
 
 
 def test_create_transfer_slot_with_depot_id_slot_id_and_retry_after() -> None:
-	depot_id = "depot1"
-	slot_id = "slot1"
+	depot_id = "depot1.uib.test"
+	client_id = "client1.uib.test"
+	slot_id = TEST_SLOT_ID
 	retry_after = 60
-	transfer_slot = TransferSlot(depot_id, slot_id, retry_after)
+	transfer_slot = TransferSlot(depot_id, client_id, slot_id, retry_after)
 	assert transfer_slot.depot_id == depot_id
-	assert transfer_slot.slot_id == slot_id
+	assert transfer_slot.client_id == client_id
+	assert transfer_slot.slot_id is None
 	assert transfer_slot.retry_after == retry_after
 
 
-def test_acquire_transfer_slot_session_error(test_client: OpsiconfdTestClient) -> None:  # pylint: disable=redefined-outer-name
-	test_client.auth = ("fake-user", "fake-pass")
-	res = test_client.post("/rpc", json={"id": 1, "method": "service_acquireTransferSlot", "params": ["depot1.uib.test"]})
+def test_transfer_slot_session_error(backend: UnprotectedBackend) -> None:  # pylint: disable=redefined-outer-name
+	with pytest.raises(BackendPermissionDeniedError) as excinfo:
+		backend.depot_acquireTransferSlot(depot="depot1.uib.test", client="client1.uib.test")
+		assert "Access denied" in str(excinfo.value)
+	with pytest.raises(BackendPermissionDeniedError) as excinfo:
+		backend.depot_releaseTransferSlot(depot="depot1.uib.test", client="client1.uib.test", slot_id=TEST_SLOT_ID)
+		assert "Access denied" in str(excinfo.value)
+
+
+def test_release_transfer_slot_with_slot_id(  # pylint: disable=redefined-outer-name
+	test_client: OpsiconfdTestClient, config: Config
+) -> None:
+	test_client.auth = (ADMIN_USER, ADMIN_PASS)
+
+	rpc = {"id": 1, "method": "depot_acquireTransferSlot", "params": ["depot1.uib.test", "client1.uib.test", TEST_SLOT_ID]}
+	res = test_client.post("/rpc", json=rpc)
+	result = res.json()
+	print(result)
+	# Assert the result
+	assert result["result"].get("slot_id") == TEST_SLOT_ID
+	assert result["result"].get("depot_id") == "depot1.uib.test"
+	assert result["result"].get("client_id") == "client1.uib.test"
+	assert result["result"].get("retry_after") is None
+
+	with sync_redis_client() as redis_client:
+		redis_res = decode_redis_result(redis_client.keys())
+		print(redis_res)
+		redis_res = decode_redis_result(redis_client.get(f"{config.redis_key('slot')}:depot1.uib.test:client1.uib.test:{TEST_SLOT_ID}"))
+		print(redis_res)
+		assert redis_res == "client1.uib.test"
+		redis_res = decode_redis_result(redis_client.ttl(f"{config.redis_key('slot')}:depot1.uib.test:client1.uib.test:{TEST_SLOT_ID}"))
+		assert 10 < redis_res <= 60
+
+	rpc = {"id": 1, "method": "depot_releaseTransferSlot", "params": ["depot1.uib.test", "client1.uib.test", TEST_SLOT_ID]}
+	res = test_client.post("/rpc", json=rpc)
 	result = res.json()
 
-	assert result["result"] is None
-	assert result["error"] == "Authentication error"
+	assert result["error"] is None
+
+	with sync_redis_client() as redis_client:
+		redis_res = decode_redis_result(redis_client.get(f"{config.redis_key('slot')}:depot1.uib.test:client1.uib.test:{TEST_SLOT_ID}"))
+		print(redis_res)
+		assert redis_res is None

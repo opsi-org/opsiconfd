798bb81cc2dfeff99c50bd096ebaf61265131fdd
Fabian Kalweit <f.kalweit@uib.de>
2023-10-26 14:21:47 +0000

    [fix] bug with symlinks and warning

--- opsiconfd/application/webdav.py
+++ opsiconfd/application/webdav.py
@@ -216,11 +216,11 @@ def webdav_setup(app: FastAPI) -> None:  # pylint: disable=too-many-statements, 
 		except Exception as err:  # pylint: disable=broad-except
 			logger.error(err, exc_info=True)
 
-	symlinks = config.allow_webdav_symlinks
+	symlinks = config.allow_webdav_symlinks or []
 	for name, conf in filesystems.items():
 		app_config = app_config_template.copy()
 		prov_class = IgnoreCaseFilesystemProvider if conf["ignore_case"] else FilesystemProvider
-		app_config["provider_mapping"]["/"] = prov_class(conf["path"], readonly=conf["read_only"])  # type: ignore[index]
+		app_config["provider_mapping"]["/"] = prov_class(conf["path"], readonly=conf["read_only"], fs_opts={})  # type: ignore[index]
 		app_config["mount_path"] = f"/{name}"
 		app.routes.append(Mount(f"/{name}", WSGIMiddleware(WsgiDAVApp(app_config))))
 
@@ -232,7 +232,7 @@ def webdav_setup(app: FastAPI) -> None:  # pylint: disable=too-many-statements, 
 			fs_opts = {"follow_symlinks": True}
 			app_config["provider_mapping"][f"/{name}"] = prov_class(conf["path"], readonly=False, fs_opts=fs_opts)  # type: ignore[index]
 		else:
-			app_config["provider_mapping"][f"/{name}"] = prov_class(conf["path"], readonly=False)  # type: ignore[index]
+			app_config["provider_mapping"][f"/{name}"] = prov_class(conf["path"], readonly=False, fs_opts={})  # type: ignore[index]
 	virt_root_provider = VirtualRootFilesystemProvider(app_config["provider_mapping"])  # type: ignore[arg-type]
 	app_config["provider_mapping"]["/"] = virt_root_provider  # type: ignore[index]
 	app_config["mount_path"] = "/dav"

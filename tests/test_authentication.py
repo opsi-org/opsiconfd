55565a968d9b2c242f508775612939f7e83cedf8
Fabian Kalweit <f.kalweit@uib.de>
2023-10-27 12:21:47 +0000

    [fix] test session max age

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -487,7 +487,7 @@ def test_session_expire(test_client: OpsiconfdTestClient) -> None:  # pylint: di
 
 
 def test_session_max_age(test_client: OpsiconfdTestClient) -> None:  # pylint: disable=redefined-outer-name,unused-argument
-	with patch("opsiconfd.session.MESSAGEBUS_IN_USE_TIMEOUT", 10):
+	with patch("opsiconfd.session.MESSAGEBUS_IN_USE_TIMEOUT", 9):
 		lifetime = 60
 		test_client.auth = (ADMIN_USER, ADMIN_PASS)
 
@@ -511,14 +511,14 @@ def test_session_max_age(test_client: OpsiconfdTestClient) -> None:  # pylint: d
 		assert session_id == cookie.value
 
 		with test_client.websocket_connect("/messagebus/v1", headers={"Cookie": f"{cookie.name}={cookie.value}"}):
-			time.sleep(1)
+			time.sleep(2)
 			res = test_client.get("/admin/", headers=lt_headers)
 			assert res.status_code == 200
 			cookie = list(test_client.cookies.jar)[0]
 			# Session cookie
 			assert cookie.expires is None
 
-		time.sleep(6)
+		time.sleep(10)
 		res = test_client.get("/admin/")
 		assert res.status_code == 200
 		cookie = list(test_client.cookies.jar)[0]

f41d27b9e8867f737d5b1c56cd838e1b8ac3e9c0
Jan Schneider <j.schneider@uib.de>
2023-11-14 17:25:21 +0000

    [fix] Fix filename in terminal on upload respone

--- opsiconfd_data/static/javascript/admininterface.js
+++ opsiconfd_data/static/javascript/admininterface.js
@@ -1255,7 +1255,7 @@ function messagebusConnect() {
 				created: Date.now(),
 				expires: Date.now() + 10000,
 				terminal_id: mbTerminal.terminalId,
-				data: utf8Encode.encode("'" + message.path + "'\033[D".repeat(message.path.length + 2))
+				data: utf8Encode.encode("'" + message.path + "'" + ("\033[D".repeat(message.path.length + 2)))
 			}
 			messagebusSend(dataMessage);
 		}

0d696034f02c091b7dd0e11ff3005efa6fdfa046
Jan Schneider <j.schneider@uib.de>
2023-11-28 15:59:08 +0000

    [fix] Fix wsgi webdav

--- opsiconfd/application/webdav.py
+++ opsiconfd/application/webdav.py
@@ -12,7 +12,6 @@ import os
 from typing import Optional
 
 import wsgidav.fs_dav_provider  # type: ignore[import]
-from a2wsgi import WSGIMiddleware
 from fastapi import FastAPI
 from fastapi.routing import Mount
 from wsgidav import util  # type: ignore[import]
@@ -36,6 +35,7 @@ from opsiconfd.config import (
 	get_depotserver_id,
 )
 from opsiconfd.logging import logger
+from opsiconfd.wsgi import WSGIMiddleware
 
 BLOCK_SIZE = 64 * 1024
 

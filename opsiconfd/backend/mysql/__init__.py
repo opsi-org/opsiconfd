cf9c4c45ecf6b7a85ab72dbb93704f3972532560
Jan Schneider <j.schneider@uib.de>
2023-12-22 18:18:08 +0000

    [chg] rollback

--- opsiconfd/backend/mysql/__init__.py
+++ opsiconfd/backend/mysql/__init__.py
@@ -104,12 +104,11 @@ class MySQLSession(Session):  # pylint: disable=too-few-public-methods
 						raise
 					str_err = str(err).lower()
 					if "server has gone away" in str_err:
-						# connection = self.connection()
+						connection = self.connection()
 						# connection.invalidate()
-						# trans = connection.get_transaction()
-						# if trans:
-						# trans.rollback()
-						pass
+						trans = connection.get_transaction()
+						if trans:
+							trans.rollback()
 					elif "deadlock" in str_err:
 						pass
 					else:

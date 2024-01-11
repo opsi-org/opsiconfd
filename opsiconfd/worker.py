99a0546521a4e4649708bbdd78bbb8e7a2a0bd9d
Jan Schneider <j.schneider@uib.de>
2024-01-11 15:37:35 +0000

    [chg,pub] Set the max length of the WebSocket incoming message queue to 500
    default: 32

--- opsiconfd/worker.py
+++ opsiconfd/worker.py
@@ -74,6 +74,7 @@ def uvicorn_config() -> Config:
 		"date_header": False,
 		"server_header": False,
 		"headers": [["Server", f"opsiconfd {__version__} (uvicorn)"], ["X-opsi-server-role", opsi_config.get("host", "server-role")]],
+		"ws_max_queue": 500,
 		"ws_ping_interval": 15,
 		"ws_ping_timeout": 10,
 	}

ec08126d3ab50e5997985a3a9c27587fc11ffee8
Jan Schneider <j.schneider@uib.de>
2024-01-17 18:20:54 +0000

    [fix] Fix packaging of opsiconfd

--- opsiconfd/__init__.py
+++ opsiconfd/__init__.py
@@ -8,7 +8,7 @@
 The opsi configuration service.
 """
 
-__version__ = "4.2.0.317"
+__version__ = "4.2.0.318"
 
 from contextvars import Context, ContextVar
 from typing import TYPE_CHECKING, Dict, Optional

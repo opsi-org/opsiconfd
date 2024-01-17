d7b5baf07997d284d02a3f72151cb5dee951ce3b
Jan Schneider <j.schneider@uib.de>
2024-01-17 17:12:45 +0000

    [chg,pub] Update python-opsi to 4.2.0.350

--- opsiconfd/__init__.py
+++ opsiconfd/__init__.py
@@ -8,7 +8,7 @@
 The opsi configuration service.
 """
 
-__version__ = "4.2.0.315"
+__version__ = "4.2.0.317"
 
 from contextvars import Context, ContextVar
 from typing import TYPE_CHECKING, Dict, Optional

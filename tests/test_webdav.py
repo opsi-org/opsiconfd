14fe81d2bcd014a65c28787deff8095da86caa68
Jan Schneider <j.schneider@uib.de>
2024-01-18 23:07:25 +0000

    [chg] Increase memory usage limits for webdav tests

--- tests/test_webdav.py
+++ tests/test_webdav.py
@@ -327,8 +327,8 @@ def test_webdav_memory_consumption(test_client: OpsiconfdTestClient) -> None:  #
 		print(f"Max memory usage upload: {usage_max_upload / 1_000_000:.2f} MB")
 		print(f"Max memory usage download: {usage_max_download / 1_000_000:.2f} MB")
 
-		assert usage_max_upload < size * 1.9
-		assert usage_max_download < size * 2.9
+		assert usage_max_upload < size * 2
+		assert usage_max_download < size * 3
 	finally:
 		mem_watch.stop.set()
 

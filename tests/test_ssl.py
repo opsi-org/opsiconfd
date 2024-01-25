9a6aa62a94cf9a10c6e26f1b96cb6fd29dbe1fb3
Jan Schneider <j.schneider@uib.de>
2024-01-25 16:44:41 +0000

    [chg,pub] Add test for additional certs in CA

--- tests/test_ssl.py
+++ tests/test_ssl.py
@@ -55,6 +55,55 @@ from opsiconfd.ssl import (
 
 from .utils import get_config
 
+GLOBALSIGN_ROOT_CA = """
+-----BEGIN CERTIFICATE-----
+MIIDdTCCAl2gAwIBAgILBAAAAAABFUtaw5QwDQYJKoZIhvcNAQEFBQAwVzELMAkG
+A1UEBhMCQkUxGTAXBgNVBAoTEEdsb2JhbFNpZ24gbnYtc2ExEDAOBgNVBAsTB1Jv
+b3QgQ0ExGzAZBgNVBAMTEkdsb2JhbFNpZ24gUm9vdCBDQTAeFw05ODA5MDExMjAw
+MDBaFw0yODAxMjgxMjAwMDBaMFcxCzAJBgNVBAYTAkJFMRkwFwYDVQQKExBHbG9i
+YWxTaWduIG52LXNhMRAwDgYDVQQLEwdSb290IENBMRswGQYDVQQDExJHbG9iYWxT
+aWduIFJvb3QgQ0EwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDaDuaZ
+jc6j40+Kfvvxi4Mla+pIH/EqsLmVEQS98GPR4mdmzxzdzxtIK+6NiY6arymAZavp
+xy0Sy6scTHAHoT0KMM0VjU/43dSMUBUc71DuxC73/OlS8pF94G3VNTCOXkNz8kHp
+1Wrjsok6Vjk4bwY8iGlbKk3Fp1S4bInMm/k8yuX9ifUSPJJ4ltbcdG6TRGHRjcdG
+snUOhugZitVtbNV4FpWi6cgKOOvyJBNPc1STE4U6G7weNLWLBYy5d4ux2x8gkasJ
+U26Qzns3dLlwR5EiUWMWea6xrkEmCMgZK9FGqkjWZCrXgzT/LCrBbBlDSgeF59N8
+9iFo7+ryUp9/k5DPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNVHRMBAf8E
+BTADAQH/MB0GA1UdDgQWBBRge2YaRQ2XyolQL30EzTSo//z9SzANBgkqhkiG9w0B
+AQUFAAOCAQEA1nPnfE920I2/7LqivjTFKDK1fPxsnCwrvQmeU79rXqoRSLblCKOz
+yj1hTdNGCbM+w6DjY1Ub8rrvrTnhQ7k4o+YviiY776BQVvnGCv04zcQLcFGUl5gE
+38NflNUVyRRBnMRddWQVDf9VMOyGj/8N7yy5Y0b2qvzfvGn9LhJIZJrglfCm7ymP
+AbEVtQwdpf5pLGkkeB6zpxxxYu7KyJesF12KwvhHhm4qxFYxldBniYUr+WymXUad
+DKqC5JlR3XC321Y9YeRq4VzW9v493kHMB65jUr9TU/Qr6cf9tveCX4XSQRjbgbME
+HMUfpIBvFSDJ3gyICh3WZlXi/EjJKSZp4A==
+-----END CERTIFICATE-----
+"""
+
+DIGICERT_GLOBAL_ROOT_CA = """
+-----BEGIN CERTIFICATE-----
+MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh
+MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
+d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD
+QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT
+MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j
+b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG
+9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB
+CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97
+nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt
+43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P
+T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4
+gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO
+BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR
+TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw
+DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr
+hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg
+06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF
+PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls
+YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk
+CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=
+-----END CERTIFICATE-----
+"""
+
 
 def test_get_ips() -> None:
 	ips = get_ips()
@@ -258,7 +307,11 @@ def test_server_key_fallback(tmpdir: Path) -> None:
 			load_local_server_key()
 
 
-def test_recreate_ca(tmpdir: Path) -> None:
+@pytest.mark.parametrize(
+	"additional_certs",
+	([], [GLOBALSIGN_ROOT_CA], [GLOBALSIGN_ROOT_CA, DIGICERT_GLOBAL_ROOT_CA]),
+)
+def test_recreate_ca(tmpdir: Path, additional_certs: list[str]) -> None:
 	ssl_ca_cert = tmpdir / "opsi-ca-cert.pem"
 	ssl_ca_key = tmpdir / "opsi-ca-key.pem"
 	config.ssl_ca_cert = str(ssl_ca_cert)
@@ -274,7 +327,12 @@ def test_recreate_ca(tmpdir: Path) -> None:
 		(ca_crt, ca_key) = create_ca(subject=subject, valid_days=valid_days)
 		store_ca_key(ca_key)
 		store_ca_cert(ca_crt)
+		if additional_certs:
+			with ssl_ca_cert.open("a", encoding="utf-8") as file:
+				file.write("\n".join(additional_certs))
+			# print(ssl_ca_cert.read_text(encoding="utf-8"))
 
+		assert load_ca_cert() == ca_crt
 		key1 = load_ca_key()
 		assert ca_key.private_bytes(
 			encoding=serialization.Encoding.PEM,
@@ -297,6 +355,11 @@ def test_recreate_ca(tmpdir: Path) -> None:
 			format=serialization.PrivateFormat.TraditionalOpenSSL,
 			encryption_algorithm=serialization.NoEncryption(),
 		)
+		if additional_certs:
+			# Additional certs must be kept
+			data = ssl_ca_cert.read_text(encoding="utf-8")
+			for additional_cert in additional_certs:
+				assert additional_cert.strip() in data
 
 		(ca_crt, ca_key) = create_ca(subject=subject, valid_days=valid_days, key=load_ca_key())
 		assert ca_key.private_bytes(
@@ -308,6 +371,11 @@ def test_recreate_ca(tmpdir: Path) -> None:
 			format=serialization.PrivateFormat.TraditionalOpenSSL,
 			encryption_algorithm=serialization.NoEncryption(),
 		)
+		if additional_certs:
+			# Additional certs must be kept
+			data = ssl_ca_cert.read_text(encoding="utf-8")
+			for additional_cert in additional_certs:
+				assert additional_cert.strip() in data
 
 		# New key
 		(ca_crt, ca_key) = create_ca(subject=subject, valid_days=valid_days)
@@ -320,9 +388,18 @@ def test_recreate_ca(tmpdir: Path) -> None:
 			format=serialization.PrivateFormat.TraditionalOpenSSL,
 			encryption_algorithm=serialization.NoEncryption(),
 		)
+		if additional_certs:
+			# Additional certs must be kept
+			data = ssl_ca_cert.read_text(encoding="utf-8")
+			for additional_cert in additional_certs:
+				assert additional_cert.strip() in data
 
 
-def test_renew_expired_ca(tmpdir: Path) -> None:  # pylint: disable=too-many-statements
+@pytest.mark.parametrize(
+	"additional_certs",
+	([], [GLOBALSIGN_ROOT_CA], [GLOBALSIGN_ROOT_CA, DIGICERT_GLOBAL_ROOT_CA]),
+)
+def test_renew_expired_ca(tmpdir: Path, additional_certs: list[str]) -> None:  # pylint: disable=too-many-statements
 	ssl_ca_cert = tmpdir / "opsi-ca-cert.pem"
 	ssl_ca_key = tmpdir / "opsi-ca-key.pem"
 	ssl_server_cert = tmpdir / "opsiconfd-cert.pem"
@@ -354,9 +431,19 @@ def test_renew_expired_ca(tmpdir: Path) -> None:  # pylint: disable=too-many-sta
 		mock.patch("opsicommon.ssl.linux._get_cert_path_and_cmd", lambda: (str(tmpdir), "echo")),
 		mock.patch("opsiconfd.ssl.get_ca_subject", lambda: ca_subject),
 	):
+		if additional_certs:
+			with ssl_ca_cert.open("w", encoding="utf-8") as file:
+				file.write("\n".join(additional_certs))
+
 		with mock.patch("opsicommon.ssl.common.CertificateBuilder", MockCertificateBuilder):
 			setup_ca()
 			setup_server_cert()
+
+		if additional_certs:
+			# Additional certs must be kept
+			data = ssl_ca_cert.read_text(encoding="utf-8")
+			for additional_cert in additional_certs:
+				assert additional_cert.strip() in data
 
 		# Check CA
 		assert (datetime.datetime.now(tz=datetime.timezone.utc) - get_ca_cert_info()["not_before"]).days >= 10
@@ -395,6 +482,12 @@ def test_renew_expired_ca(tmpdir: Path) -> None:  # pylint: disable=too-many-sta
 			encryption_algorithm=serialization.NoEncryption(),
 		)
 
+		if additional_certs:
+			# Additional certs must be kept
+			data = ssl_ca_cert.read_text(encoding="utf-8")
+			for additional_cert in additional_certs:
+				assert additional_cert.strip() in data
+
 		# Subject must stay the same
 		ca_crt = load_ca_cert()
 		assert x509_name_to_dict(ca_crt.subject) == orig_ca_subject
@@ -418,6 +511,12 @@ def test_renew_expired_ca(tmpdir: Path) -> None:  # pylint: disable=too-many-sta
 		)
 		# Subject must stay the same
 		assert x509_name_to_dict(ca_crt.subject) == orig_ca_subject
+
+		if additional_certs:
+			# Additional certs must be kept
+			data = ssl_ca_cert.read_text(encoding="utf-8")
+			for additional_cert in additional_certs:
+				assert additional_cert.strip() in data
 
 		# Check server cert validity
 		with pytest.raises(verification.VerificationError, match=r"CA is not valid before.*but certificate is valid before"):

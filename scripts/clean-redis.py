bdb2d78dd56999c70530c52b5115b3c88a619b80
Jan Schneider <j.schneider@uib.de>
2024-01-31 07:39:30 +0100

    [chg] Readd scripts/clean-redis.py

---
+++ scripts/clean-redis.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# opsiconfd is part of the desktop management solution opsi http://www.opsi.org
+# Copyright (c) 2023 uib GmbH <info@uib.de>
+# All rights reserved.
+# License: AGPL-3.0
+
+"""
+poetry run python scripts/clean-redis.py
+"""
+import os
+import redis
+
+redis_url = os.environ.get("OPSICONFD_REDIS_INTERNAL_URL") or "redis://localhost"
+print(f"Clean Redis DB: {redis_url}")
+
+redis_client = redis.StrictRedis.from_url(redis_url)
+keys = redis_client.scan_iter("opsiconfd*")
+for key in keys:
+	print(f"Delete key: {key.decode('utf-8')}")
+	redis_client.delete(key)
+
+print("Done")

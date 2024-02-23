e0aaa503a3d3e5d3dbb1966f97d2aa81a0bb474d
Jan Schneider <j.schneider@uib.de>
2024-02-23 08:08:58 +0100

    [chg] Cleanup dynamic depot selection code

--- opsiconfd/backend/rpc/ext_dynamic_depot.py
+++ opsiconfd/backend/rpc/ext_dynamic_depot.py
@@ -26,41 +26,20 @@ SHOW_DEPOT_INFO_FUNCTION = """
 			logger.info("   Alternative depot: %s", alternativeDepot)
 """
 
-GET_DEPOT_WITH_LOWEST_LATENCY_FUNCTION = '''
-	def getDepotWithLowestLatency(latency):
-		"""
-		Given a dict with depot as key and latency as value it will \
-return the depot with the lowest latency.
-
-		Will return None if no depot can be determined.
-		"""
-		selectedDepot = None
-		if latency:
-			minValue = 1000
-			for (depot, value) in latency.items():
-				if value < minValue:
-					minValue = value
-					selectedDepot = depot
-			logger.notice("Depot with lowest latency: %s (%0.3f ms)", selectedDepot, minValue*1000)
-
-		return selectedDepot
-'''
-
 GET_LATENCY_INFORMATION_FUNCTION = '''
 	def getLatencyInformation(depots):
 		"""
-		Pings the given depots and returns the latency information in \
-a dict with depot as key and the latency as value.
+		Pings the given depots and returns the latency information in a list of tuples with depot as first and the latency as second item.
 
 		Depots that can't be reached in time will not be included.
 		"""
 		from OPSI.Util.Ping import ping
 		from urllib.parse import urlparse
 
-		latency = {}
+		latency = []
 		for depot in depots:
 			if not depot.repositoryRemoteUrl:
-				logger.info("Skipping {depot} because repositoryRemoteUrl is missing.", depot)
+				logger.info("Skipping %r because repositoryRemoteUrl is missing.", depot)
 				continue
 
 			try:
@@ -74,11 +53,26 @@ a dict with depot as key and the latency as value.
 					logger.info("Ping to depot %s timed out.", depot)
 				else:
 					logger.info("Latency of depot %s: %0.3f ms", depot, depotLatency * 1000)
-					latency[depot] = depotLatency
-			except Exception as e:
-				logger.warning(e)
+					latency.append((depot, depotLatency))
+			except Exception as err:
+				logger.warning(err)
 
 		return latency
+'''
+
+GET_DEPOT_WITH_LOWEST_LATENCY_FUNCTION = '''
+	def getDepotWithLowestLatency(latency):
+		"""
+		Given a list of tuples with depot as first and latency as second item it will return the depot with the lowest latency.
+
+		Will return None if no depot can be determined.
+		"""
+		if not latency:
+			return None
+
+		selectedDepot, minLatency = sorted(latency, key=lambda x: x[1])[0]
+		logger.notice("Depot with lowest latency: %r (%0.3f ms)", selectedDepot, minLatency * 1000)
+		return selectedDepot
 '''
 
 DEPOT_SELECTION_ALGORITHM_BY_MASTER_DEPOT_AND_LATENCY = f"""
@@ -89,31 +83,28 @@ def selectDepot(clientConfig, masterDepot, alternativeDepots=[]):
 
 	showDepotInfo()
 
-	if alternativeDepots:
-		from collections import defaultdict
+	if not alternativeDepots:
+		return masterDepot
 
-		# Mapping of depots to its master.
-		# key: Master depot
-		# value: All slave depots + master
-		depotsByMaster = defaultdict(list)
+	from collections import defaultdict
 
-		allDepots = [masterDepot] + alternativeDepots
+	# Mapping of depots to its master.
+	# key: Master depot
+	# value: All slave depots + master
+	depotsByMaster = defaultdict(list)
 
-		for depot in allDepots:
-			if depot.masterDepotId:
-				depotsByMaster[depot.masterDepotId].append(depot)
-			else:
-				depotsByMaster[depot.id].append(depot)
+	for depot in [masterDepot] + alternativeDepots:
+		if depot.masterDepotId:
+			depotsByMaster[depot.masterDepotId].append(depot)
+		else:
+			depotsByMaster[depot.id].append(depot)
 
-		depotsWithLatency = getLatencyInformation(depotsByMaster[masterDepot.id])
-		depotWithLowestLatency = getDepotWithLowestLatency(depotsWithLatency)
-
-		if not depotWithLowestLatency:
-			logger.info('No depot with lowest latency. Falling back to master depot.')
-			depotWithLowestLatency = masterDepot
-
+	depotsWithLatency = getLatencyInformation(depotsByMaster[masterDepot.id])
+	depotWithLowestLatency = getDepotWithLowestLatency(depotsWithLatency)
+	if depotWithLowestLatency:
 		return depotWithLowestLatency
 
+	logger.info("No depot with lowest latency, falling back to master depot.")
 	return masterDepot
 """
 
@@ -125,16 +116,16 @@ def selectDepot(clientConfig, masterDepot, alternativeDepots=[]):
 
 	showDepotInfo()
 
-	selectedDepot = masterDepot
-	if alternativeDepots:
-		depotsWithLatency = getLatencyInformation([masterDepot] + alternativeDepots)
-		selectedDepot = getDepotWithLowestLatency(depotsWithLatency)
+	if not alternativeDepots:
+		return masterDepot
 
-		if not selectedDepot:
-			logger.info('No depot with lowest latency. Falling back to master depot.')
-			selectedDepot = masterDepot
+	depotsWithLatency = getLatencyInformation([masterDepot] + alternativeDepots)
+	depotWithLowestLatency = getDepotWithLowestLatency(depotsWithLatency)
+	if depotWithLowestLatency:
+		return depotWithLowestLatency
 
-	return selectedDepot
+	logger.info("No depot with lowest latency, falling back to master depot.")
+	return masterDepot
 """
 
 DEPOT_SELECTION_ALGORITHM_BY_RANDOM = f"""
@@ -144,10 +135,9 @@ def selectDepot(clientConfig, masterDepot, alternativeDepots=[]):
 	showDepotInfo()
 
 	import random
-
-	allDepots = [masterDepot]
-	allDepots.extend(alternativeDepots)
-	return random.choice(allDepots)
+	selectedDepot = random.choice([masterDepot] + alternativeDepots)
+	logger.info("The depot %r was selected at random", selectedDepot)
+	return selectedDepot
 """
 
 
@@ -157,28 +147,22 @@ def selectDepot(clientConfig, masterDepot, alternativeDepots=[]):
 
 	showDepotInfo()
 	logger.debug("Alternative Depots are: %s", alternativeDepots)
-	selectedDepot = masterDepot
-	if alternativeDepots:
-		try:
-			from opsicommon.utils import ip_address_in_network
-		except ImportError:
-			from OPSI.Util import ipAddressInNetwork as ip_address_in_network
 
-		depots = [masterDepot]
-		depots.extend(alternativeDepots)
-		for depot in depots:
-			if not depot.networkAddress:
-				logger.warning("Network address of depot '%s' not known", depot)
-				continue
+	if not alternativeDepots:
+		return masterDepot
 
-			if ip_address_in_network(clientConfig['ipAddress'], depot.networkAddress):
-				logger.notice("Choosing depot with networkAddress %s for ip %s", depot.networkAddress, clientConfig['ipAddress'])
-				selectedDepot = depot
-				break
-			else:
-				logger.info("IP %s does not match networkAddress %s of depot %s", clientConfig['ipAddress'], depot.networkAddress, depot)
+	try:
+		from opsicommon.utils import ip_address_in_network
+	except ImportError:
+		from OPSI.Util import ipAddressInNetwork as ip_address_in_network
 
-	return selectedDepot
+	for depot in sorted([d for d in ([masterDepot] + alternativeDepots) if d.networkAddress], key=lambda x: ipaddress.ip_network(x.networkAddress).prefixlen, reverse=True):
+		if ip_address_in_network(clientConfig['ipAddress'], depot.networkAddress):
+			logger.notice("Choosing depot with networkAddress %s for ip %s", depot.networkAddress, clientConfig['ipAddress'])
+			return depot
+		logger.info("IP %s does not match networkAddress %s of depot %s", clientConfig['ipAddress'], depot.networkAddress, depot)
+
+	return masterDepot
 """
 
 

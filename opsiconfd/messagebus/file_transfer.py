65d7f7c8a1609d72232f837d7892e1c94fcdbb80
Jan Schneider <j.schneider@uib.de>
2024-03-22 14:23:21 +0100

    [chg] Add messagebus/file_transfer.py

---
+++ opsiconfd/messagebus/file_transfer.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# opsiconfd is part of the desktop management solution opsi http://www.opsi.org
+# Copyright (c) 2020-2022 uib GmbH <info@uib.de>
+# All rights reserved.
+# License: AGPL-3.0
+"""
+opsiconfd.messagebus.filetransfer
+"""
+
+from __future__ import annotations
+
+from opsicommon.messagebus.file_transfer import stop_running_file_transfers
+
+
+async def async_file_transfer_shutdown() -> None:
+	await stop_running_file_transfers()

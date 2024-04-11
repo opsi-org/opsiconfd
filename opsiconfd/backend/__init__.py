765904b678a542ba988937d1c55c20772553c325
Jan Schneider <j.schneider@uib.de>
2024-04-12 00:22:58 +0200

    [chg] Run messagebus_connection_established callback
    if messagebus is already connected when registering a listener

--- opsiconfd/backend/__init__.py
+++ opsiconfd/backend/__init__.py
@@ -122,6 +122,10 @@ def get_service_client(name: str = "", register_messagebus_listener: MessagebusL
 			service_clients[name] = service_client
 		elif register_messagebus_listener:
 			service_clients[name].messagebus.register_messagebus_listener(register_messagebus_listener)
+			if service_clients[name].messagebus.connected:
+				service_clients[name].messagebus._run_listener_callback(
+					register_messagebus_listener, "messagebus_connection_established", messagebus=service_clients[name].messagebus
+				)
 
 		return service_clients[name]
 

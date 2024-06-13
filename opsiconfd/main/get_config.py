171fd17c149e95d6f63a5aa4472b111f494b42c3
Fabian Kalweit <f.kalweit@uib.de>
2024-06-13 11:01:41 +0200

    [pub,chg] only allow configed versions >= 4.3.2.18 to connect

--- opsiconfd/main/get_config.py
+++ opsiconfd/main/get_config.py
@@ -12,7 +12,8 @@ opsiconfd config.main
 import json
 
 from opsiconfd.config import config
+from opsiconfd.utils import VersionEncoder
 
 
 def get_config_main() -> None:
-	print(json.dumps(config.items(), indent=2))
+	print(json.dumps(config.items(), indent=2, cls=VersionEncoder))

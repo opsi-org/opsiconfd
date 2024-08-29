f1c8b891b802c3e622535b34b437545146f040ff
Jan Schneider <j.schneider@uib.de>
2024-08-29 15:28:16 +0200

    [chg,pub] Abort startup if server id in opsi.conf differs from database

---
+++ opsiconfd/exception.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# opsiconfd is part of the desktop management solution opsi http://www.opsi.org
+# Copyright (c) 2008-2024 uib GmbH <info@uib.de>
+# All rights reserved.
+# License: AGPL-3.0
+
+"""
+opsiconfd.exception
+"""
+
+
+class OpsiconfdException(Exception):
+	pass
+
+
+class ConfigurationError(OpsiconfdException):
+	pass

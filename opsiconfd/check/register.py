7bd39f36d5e8a2f0dbef8624ecd45df242efd169
Fabian Kalweit <f.kalweit@uib.de>
2024-09-19 14:23:39 +0200

    [chg] add config checks

--- opsiconfd/check/register.py
+++ opsiconfd/check/register.py
@@ -6,6 +6,7 @@ __all__ = ["register_checks"]
 def register_checks() -> None:
 	import opsiconfd.check.backend  # noqa: F401
 	import opsiconfd.check.backup  # noqa: F401
+	import opsiconfd.check.config  # noqa: F401
 	import opsiconfd.check.ssl  # noqa: F401
 
 	print(check_manager.check_ids)

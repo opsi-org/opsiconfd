064bbbc0a1beb83d18c43b43b113862face50005
Fabian Kalweit <f.kalweit@uib.de>
2024-09-24 13:34:57 +0200

    [chg] opsiconfd: Refactor Redis checks

--- opsiconfd/check/redis.py
+++ opsiconfd/check/redis.py
@@ -9,78 +9,126 @@
 # health check
 # """
 
-# from opsiconfd.check.common import CheckResult, CheckStatus, PartialCheckResult, exc_to_result,  Check
-# from opsiconfd.logging import logger
-# from opsiconfd.redis import decode_redis_result, redis_client
+from dataclasses import dataclass
 
-# MEMORY_USAGE_WARN = 300_000_000
-# MEMORY_USAGE_ERR = 500_000_000
+from opsiconfd.check.common import Check, CheckResult, CheckStatus, check_manager, exc_to_result
+from opsiconfd.redis import decode_redis_result, redis_client
+
+MEMORY_USAGE_WARN = 300_000_000
+MEMORY_USAGE_ERR = 500_000_000
 
 
-# class RedisCheck(Check):
-# 	def check(self) -> CheckResult:
-# 		result = self.result
-# 		with exc_to_result(result):
-# 			redis = redis_client(timeout=5, test_connection=True)
-# 			result.add_partial_result(
-# 				PartialCheckResult(
-# 					check_id="redis:connection",
-# 					check_name="Redis connection",
-# 					check_status=CheckStatus.OK,
-# 					message="Connection to Redis is working.",
-# 				)
-# 			)
+@dataclass()
+class RedisMemoryUsageCheck(Check):
+	id: str = "redis:memory_usage"
+	name: str = "Redis memory usage"
+	description: str = "Check whether the Redis memory usage is not too high."
+	partial_check: bool = True
+	documentation: str = """
+	## Redis memory usage
 
-# 			partial_result = PartialCheckResult(
-# 				check_id="redis:timeseries",
-# 				check_name="RedisTimeSeries module",
-# 				check_status=CheckStatus.ERROR,
-# 				message="RedisTimeSeries not loaded.",
-# 			)
+	Checks whether the Redis memory usage is not too high.
+	"""
 
-# 			redis_info = decode_redis_result(redis.execute_command("INFO"))
-# 			logger.debug("Redis info: %s", redis_info)
-# 			for module in redis_info.get("modules", []):
-# 				if module["name"] == "timeseries":
-# 					partial_result.check_status = CheckStatus.OK
-# 					partial_result.message = f"RedisTimeSeries version {module['ver']!r} is loaded."
-# 					partial_result.details = {"version": module["ver"]}
-# 				result.add_partial_result(partial_result)
+	def check(self) -> CheckResult:
+		result = CheckResult(
+			check=self,
+			message="Redis memory usage is OK.",
+			check_status=CheckStatus.OK,
+		)
+		with exc_to_result(result):
+			redis = redis_client(timeout=5, test_connection=True)
+			info = decode_redis_result(redis.execute_command("INFO"))
+			if info["used_memory"] >= MEMORY_USAGE_ERR:
+				result.check_status = CheckStatus.ERROR
+				result.message = f"Redis memory usage is too high: {info['used_memory_human']}"
+			elif info["used_memory"] >= MEMORY_USAGE_WARN:
+				result.check_status = CheckStatus.WARNING
+				result.message = f"Redis memory usage is high: {info['used_memory_human']}"
+			else:
+				result.message = f"Redis memory usage is OK: {info['used_memory_human']}"
+		return result
 
-# 			partial_result = PartialCheckResult(
-# 				check_id="redis:memory_usage",
-# 				check_name="Redis memory usage",
-# 				check_status=CheckStatus.OK,
-# 			)
-# 			info = redis.execute_command("INFO")
-# 			if info["used_memory"] >= MEMORY_USAGE_ERR:
-# 				partial_result.check_status = CheckStatus.ERROR
-# 				partial_result.message = f"Redis memory usage is too high: {info['used_memory_human']}"
-# 			elif info["used_memory"] >= MEMORY_USAGE_WARN:
-# 				partial_result.check_status = CheckStatus.WARNING
-# 				partial_result.message = f"Redis memory usage is high: {info['used_memory_human']}"
-# 			else:
-# 				partial_result.message = f"Redis memory usage is OK: {info['used_memory_human']}"
-# 			result.add_partial_result(partial_result)
 
-# 			if result.check_status != CheckStatus.OK:
-# 				result.message = "Some issues found with Redis."
-# 		return result
+@dataclass()
+class RedisTimeseriesCheck(Check):
+	id: str = "redis:timeseries"
+	name: str = "RedisTimeSeries module"
+	description: str = "Check whether the RedisTimeSeries module is loaded."
+	partial_check: bool = True
+	documentation: str = """
+	## RedisTimeSeries module
 
-# docs = """
-# ## Redis server
+	Checks whether the RedisTimeSeries module is loaded.
+	"""
 
-# Checks whether the Redis server is available and whether the RedisTimeSeries module is loaded.
-# If the server is not available or the module is not loaded, this is considered an error.
-# Also checks whether the memory usage is not too high.
-# """
+	def check(self) -> CheckResult:
+		result = CheckResult(
+			check=self,
+			message="RedisTimeSeries not loaded.",
+			check_status=CheckStatus.ERROR,
+		)
+		with exc_to_result(result):
+			redis = redis_client(timeout=5, test_connection=True)
+			redis_info = decode_redis_result(redis.execute_command("INFO"))
+			for module in redis_info.get("modules", []):
+				if module["name"] == "timeseries":
+					result.message = f"RedisTimeSeries version {module['ver']!r} is loaded."
+					result.check_status = CheckStatus.OK
+					result.details = {"version": module["ver"]}
+		return result
 
-# redis_check = RedisCheck(
-# 	id="redis",
-# 	name="Redis Server",
-# 	description="Check Redis server state.",
-# 	documentation=docs,
-# 	status=CheckStatus.OK,
-# 	message="No Redis issues found.",
-# 	depot_check=True,
-# )
+
+@dataclass()
+class RedisConnectionCheck(Check):
+	id: str = "redis:connection"
+	name: str = "Redis Connection"
+	description: str = "Checks whether opsi can connect to the configured Redis server."
+	partial_check: bool = True
+	documentation: str = """
+	## Check Redis Connection
+
+	Checks whether opsi can connect to the configured Redis server.
+	"""
+
+	def check(self) -> CheckResult:
+		result = CheckResult(
+			check=self,
+			message="Redis authentication is not configured.",
+			check_status=CheckStatus.OK,
+		)
+		with exc_to_result(result):
+			redis_client(timeout=5, test_connection=True)
+			result.message = "The connection to the Redis server does work."
+		return result
+
+
+@dataclass()
+class RedisCheck(Check):
+	id: str = "redis"
+	name: str = "Redis Server"
+	description: str = "Check Redis server state."
+	documentation: str = """
+	## Redis server
+
+	Checks whether the Redis server is available and whether the RedisTimeSeries module is loaded.
+	If the server is not available or the module is not loaded, this is considered an error.
+	Also checks whether the memory usage is not too high.
+	"""
+	status: CheckStatus = CheckStatus.OK
+	message: str = "No Redis issues found."
+	depot_check: bool = True
+
+	def check(self) -> CheckResult:
+		result = CheckResult(
+			check=self,
+			message="No Redis issues found.",
+			check_status=CheckStatus.OK,
+		)
+
+		return result
+
+
+redis_check = RedisCheck()
+redis_check.add_partial_checks(RedisConnectionCheck(), RedisTimeseriesCheck(), RedisMemoryUsageCheck())
+check_manager.register(redis_check)

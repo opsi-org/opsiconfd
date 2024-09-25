f92bce3445411f0c907bc64cc88d59f4df6d4ebb
Fabian Kalweit <f.kalweit@uib.de>
2024-09-25 14:29:02 +0200

    [chg] add opsipackages check

--- opsiconfd/check/register.py
+++ opsiconfd/check/register.py
@@ -1,5 +1,3 @@
-from opsiconfd.check.common import check_manager
-
 __all__ = ["register_checks"]
 
 
@@ -11,8 +9,7 @@ def register_checks() -> None:
 	import opsiconfd.check.ldap  # noqa: F401
 	import opsiconfd.check.mysql  # noqa: F401
 	import opsiconfd.check.opsilicense  # noqa: F401
+	import opsiconfd.check.opsipackages  # noqa: F401
 	import opsiconfd.check.redis  # noqa: F401
 	import opsiconfd.check.ssl  # noqa: F401
 	import opsiconfd.check.system  # noqa: F401
-
-	print(check_manager.check_ids)

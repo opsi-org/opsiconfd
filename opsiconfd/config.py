824c1242dabab4eb02097329bbe59cc352ffa111
Jan Schneider <j.schneider@uib.de>
2024-11-30 11:01:15 +0100

    [chg] Add saml_sp_private_key to secret_filter

--- opsiconfd/config.py
+++ opsiconfd/config.py
@@ -402,7 +402,9 @@ class Config(metaclass=Singleton):
 		else:
 			self._config.ssl_ca_permitted_domains = []
 
-		secret_filter.add_secrets(self._config.ssl_ca_key_passphrase, self._config.ssl_server_key_passphrase)
+		secret_filter.add_secrets(
+			self._config.ssl_ca_key_passphrase, self._config.ssl_server_key_passphrase, self._config.saml_sp_private_key
+		)
 
 		try:
 			if self._config.password:

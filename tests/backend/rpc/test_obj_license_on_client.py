b5a799ad76d5636c8c5b1dc405800a6f0ca50c88
Jan Schneider <j.schneider@uib.de>
2025-04-02 15:09:32 +0200

    [chg] Improve tests for licenseOnClient_getOrCreateObject

--- tests/backend/rpc/test_obj_license_on_client.py
+++ tests/backend/rpc/test_obj_license_on_client.py
@@ -9,76 +9,85 @@
 test opsiconfd.backend.rpc.obj_license_on_client
 """
 
+import pytest
+from opsicommon.exceptions import LicenseMissingError
 from opsicommon.objects import (
 	LicenseContract,
+	LicenseOnClient,
 	LicensePool,
+	LocalbootProduct,
 	OEMSoftwareLicense,
 	OpsiClient,
+	RetailSoftwareLicense,
+	SoftwareLicense,
 	SoftwareLicenseToLicensePool,
+	VolumeSoftwareLicense,
 )
 
-from tests.utils import (  # noqa: F401
-	ADMIN_PASS,
-	ADMIN_USER,
-	OpsiconfdTestClient,
-	clean_mysql,
-	clean_redis,
-	test_client,
+from tests.utils import UnprotectedBackend, backend, clean_mysql, clean_redis  # noqa: F401
+
+
+@pytest.mark.parametrize(
+	"license_type",
+	(OEMSoftwareLicense, VolumeSoftwareLicense, RetailSoftwareLicense),
 )
-
-from .test_obj_product import create_test_products
-
-
 def test_licenseOnClient_getOrCreateObject(
-	test_client: OpsiconfdTestClient,  # noqa: F811
+	backend: UnprotectedBackend,  # noqa: F811
+	license_type: type[SoftwareLicense],
 ) -> None:
-	test_client.auth = (ADMIN_USER, ADMIN_PASS)
-
-	product1, product2 = create_test_products(test_client)
-	pool1 = LicensePool(id="test-pool-1", description="Test pool", productIds=[product1["id"], product2["id"]])
+	max_installations = 1 if license_type is OEMSoftwareLicense else 3
+	product1 = LocalbootProduct(
+		id="test-backend-rpc-product-1",
+		productVersion="5.3.0",
+		packageVersion="2",
+		licenseRequired=True,
+		setupScript="setup.opsiscript",
+		uninstallScript="uninstall.opsiscript",
+	)
+	product2 = LocalbootProduct(
+		id="test-backend-rpc-product-2",
+		productVersion="1.2.3",
+		packageVersion="1",
+		licenseRequired=True,
+		setupScript="setup.opsiscript",
+		uninstallScript="uninstall.opsiscript",
+	)
+	pool1 = LicensePool(id="test-pool-1", description="Test pool", productIds=[product1.id, product2.id])
 	contract1 = LicenseContract(id="test-contract-1", description="test contract")
-	license1 = OEMSoftwareLicense(id="test-license-1", licenseContractId=contract1.id, maxInstallations=1)
+	license1 = license_type(id="test-license-1", licenseContractId=contract1.id, maxInstallations=3)
 	lic2pool = SoftwareLicenseToLicensePool(softwareLicenseId=license1.id, licensePoolId=pool1.id, licenseKey="key")
 	client1 = OpsiClient(id="test-client-1.opsi.org")
 	client2 = OpsiClient(id="test-client-2.opsi.org")
-	rpcs = [
-		{"jsonrpc": "2.0", "id": 1, "method": "licensePool_createObjects", "params": [pool1.to_hash()]},
-		{"jsonrpc": "2.0", "id": 2, "method": "licenseContract_createObjects", "params": [contract1.to_hash()]},
-		{"jsonrpc": "2.0", "id": 3, "method": "softwareLicense_createObjects", "params": [license1.to_hash()]},
-		{"jsonrpc": "2.0", "id": 4, "method": "softwareLicenseToLicensePool_createObjects", "params": [lic2pool.to_hash()]},
-		{"jsonrpc": "2.0", "id": 5, "method": "host_createObjects", "params": [[client1.to_hash(), client2.to_hash()]]},
-	]
-	response = test_client.post("/rpc", json=rpcs).json()
-	assert len(response) == 5
-	for res in response:
-		assert "error" not in res
+	client3 = OpsiClient(id="test-client-3.opsi.org")
+	client4 = OpsiClient(id="test-client-4.opsi.org")
 
-	rpc = {
-		"jsonrpc": "2.0",
-		"id": 1,
-		"method": "licenseOnClient_getOrCreateObject",
-		"params": {"clientId": client1.id, "productId": product1["id"]},
-	}
-	response = test_client.post("/rpc", json=rpc).json()
-	assert "error" not in response
+	backend.host_createObjects([client1, client2, client3, client4])
+	backend.product_createObjects([product1, product2])
+	backend.licensePool_createObjects([pool1])
+	backend.licenseContract_createObjects([contract1])
+	backend.softwareLicense_createObjects([license1])
+	backend.softwareLicenseToLicensePool_createObjects([lic2pool])
 
-	res = response["result"]
-	assert res["type"] == "LicenseOnClient"
-	assert res["licenseKey"] == lic2pool.licenseKey
-	assert res["softwareLicenseId"] == license1.id
-	assert res["licensePoolId"] == pool1.id
-	assert res["clientId"] == client1.id
+	for _ in range(2):
+		# Acquire license and reacquire it
+		clients = (client1, client2, client3) if max_installations == 3 else (client1,)
+		for client in clients:
+			license_on_client = backend.licenseOnClient_getOrCreateObject(
+				clientId=client.id, productId=(product2 if client == client2 else product1).id
+			)
+			assert isinstance(license_on_client, LicenseOnClient)
+			assert license_on_client.clientId == client.id
+			assert license_on_client.softwareLicenseId == license1.id
+			assert license_on_client.licenseKey == lic2pool.licenseKey
+			assert license_on_client.licensePoolId == pool1.id
 
-	response = test_client.post("/rpc", json=rpc).json()
-	assert "error" not in response
-	assert res["softwareLicenseId"] == license1.id
-	assert res["licenseKey"] == lic2pool.licenseKey
-
-	rpc = {
-		"jsonrpc": "2.0",
-		"id": 1,
-		"method": "licenseOnClient_getOrCreateObject",
-		"params": {"clientId": client2.id, "productId": product1["id"]},
-	}
-	response = test_client.post("/rpc", json=rpc).json()
-	assert response["error"]["data"]["class"] == "LicenseMissingError"
+	for product in (product1, product2):
+		# Acquire license for client4 which will fail
+		with pytest.raises(
+			LicenseMissingError,
+			match=(
+				r"License missing error: No license available for pool 'test-pool-1' and client 'test-client-4.opsi.org',"
+				r" or all remaining licenses are bound to a different host."
+			),
+		):
+			backend.licenseOnClient_getOrCreateObject(clientId=client4.id, productId=product.id)

a4dc5cadc65fd7f2660b1dcbd9f66ca41ac4dd66
Jan Schneider <j.schneider@uib.de>
2025-10-07 11:01:19 +0200

    [fix] Handle fork in terminal messagebus worker

--- opsiconfd/messagebus/terminal.py
+++ opsiconfd/messagebus/terminal.py
@@ -9,6 +9,7 @@ opsiconfd.messagebus.terminal
 
 from __future__ import annotations
 
+import os
 from queue import Empty, Queue
 
 from opsicommon.client.opsiservice import Messagebus, MessagebusListener
@@ -157,7 +158,7 @@ async def messagebus_terminal_open_request_worker_configserver() -> None:
 	messagebus_worker_id = get_messagebus_worker_id()
 
 	channel = f"service:depot:{get_depotserver_id()}:terminal"
-
+	pid = os.getpid()
 	# ID "0" means: Start reading pending messages (not ACKed) and continue reading new messages
 	terminal_request_reader = ConsumerGroupMessageReader(
 		consumer_group=channel,
@@ -165,8 +166,9 @@ async def messagebus_terminal_open_request_worker_configserver() -> None:
 	)
 	await terminal_request_reader.set_channels({channel: "0"})
 	async for redis_id, message, _context in terminal_request_reader.get_messages():
+		is_terminal_open_request = isinstance(message, TerminalOpenRequestMessage)
 		try:
-			if isinstance(message, TerminalOpenRequestMessage):
+			if is_terminal_open_request:
 				await process_messagebus_message(
 					message=message,
 					send_message=redis_send_message,
@@ -177,6 +179,9 @@ async def messagebus_terminal_open_request_worker_configserver() -> None:
 				raise ValueError(f"Received invalid message type {message.type}")
 		except Exception as err:
 			logger.error(err, exc_info=True)
+		if is_terminal_open_request and pid != os.getpid():
+			# Process has been forked, stop processing messages
+			break
 		# ACK Message
 		await terminal_request_reader.ack_message(message.channel, redis_id)
 
